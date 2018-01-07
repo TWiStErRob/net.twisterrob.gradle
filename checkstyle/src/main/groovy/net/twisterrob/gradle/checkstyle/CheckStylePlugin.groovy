93ce21f5e71610a31d040d4c5353188e79b3ea02
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-01-07 15:27:18 +0000

    Extract Android apply logic to separate method

--- checkstyle/src/main/groovy/net/twisterrob/gradle/checkstyle/CheckStylePlugin.groovy
+++ checkstyle/src/main/groovy/net/twisterrob/gradle/checkstyle/CheckStylePlugin.groovy
@@ -1,10 +1,9 @@
 package net.twisterrob.gradle.checkstyle
 
-import com.android.build.gradle.AppExtension
-import com.android.build.gradle.FeatureExtension
-import com.android.build.gradle.LibraryExtension
-import com.android.build.gradle.TestExtension
+import com.android.build.gradle.api.BaseVariant
+import net.twisterrob.gradle.common.AndroidVariantApplier
 import net.twisterrob.gradle.common.BaseExposedPlugin
+import org.gradle.api.DomainObjectSet
 import org.gradle.api.Project
 
 class CheckStylePlugin extends BaseExposedPlugin {
@@ -12,26 +11,12 @@ class CheckStylePlugin extends BaseExposedPlugin {
 	@Override
 	void apply(Project target) {
 		super.apply(target)
-
-		project.plugins.withId('com.android.application') {
-			def android = project.extensions['android'] as AppExtension
-			new CheckStyleTaskCreator(project).applyTo(android.applicationVariants)
-		}
-		project.plugins.withId('com.android.library') {
-			def android = project.extensions['android'] as LibraryExtension
-			new CheckStyleTaskCreator(project).applyTo(android.libraryVariants)
-		}
-		project.plugins.withId('com.android.feature') {
-			def android = project.extensions['android'] as FeatureExtension
-			new CheckStyleTaskCreator(project).applyTo(android.libraryVariants)
-		}
-		project.plugins.withId('com.android.test') {
-			def android = project.extensions['android'] as TestExtension
-			new CheckStyleTaskCreator(project).applyTo(android.applicationVariants)
-		}
-		project.plugins.withId('com.android.instantapp') {
-			// has no variants
-			//def android = project.extensions['android'] as InstantAppExtension
+		// level of indirection with base is to prevent loading classes in project not having Android
+		project.plugins.withId('com.android.base') {
+			new AndroidVariantApplier(project)
+					.apply({DomainObjectSet<BaseVariant> variants ->
+				new CheckStyleTaskCreator(project).applyTo(variants)
+			})
 		}
 	}
 }
---
+++ common/src/main/groovy/net/twisterrob/gradle/common/AndroidVariantApplier.groovy
@@ -0,0 +1,57 @@
+package net.twisterrob.gradle.common
+
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.FeatureExtension
+import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.TestExtension
+import com.android.build.gradle.api.BaseVariant
+import org.gradle.api.Action
+import org.gradle.api.DomainObjectSet
+import org.gradle.api.Project
+
+class AndroidVariantApplier {
+
+	private Project project
+
+	AndroidVariantApplier(Project project) {
+		this.project = project
+	}
+
+	void apply(Action<DomainObjectSet<? extends BaseVariant>> variantsClosure) {
+		project.plugins.withId('com.android.application') {
+			def android = project.extensions['android'] as AppExtension
+			variantsClosure.execute(android.applicationVariants)
+		}
+		project.plugins.withId('com.android.library') {
+			def android = project.extensions['android'] as LibraryExtension
+			variantsClosure.execute(android.libraryVariants)
+		}
+		project.plugins.withId('com.android.feature') {
+			def android = project.extensions['android'] as FeatureExtension
+			variantsClosure.execute(android.libraryVariants)
+		}
+		project.plugins.withId('com.android.test') {
+			def android = project.extensions['android'] as TestExtension
+			variantsClosure.execute(android.applicationVariants)
+		}
+		project.plugins.withId('com.android.instantapp') {
+			//def android = project.extensions['android'] as InstantAppExtension
+			// has no variants, but don't call back, because there's no way to tell if this happened
+			//variantsClosure.execute(new DefaultDomainObjectSet<>(BaseVariant))
+		}
+	}
+
+	/**
+	 * Note: due to the way {@link DomainObjectSet#all} works,
+	 * {@code after} is not really after all items have been processed.
+	 */
+	void applyAll(Action<? extends BaseVariant> variantClosure,
+			Action<DomainObjectSet<? extends BaseVariant>> before = Closure.IDENTITY as Action,
+			Action<DomainObjectSet<? extends BaseVariant>> after = Closure.IDENTITY as Action) {
+		apply({DomainObjectSet<? extends BaseVariant> variants ->
+			before.execute(variants)
+			variants.all variantClosure
+			after.execute(variants)
+		})
+	}
+}
