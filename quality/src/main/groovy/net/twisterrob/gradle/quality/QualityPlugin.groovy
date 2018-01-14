8dae16169400c47c7de04789cd1915d37927a018
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-01-14 12:23:57 +0000

    Apply PMD with quality plugin

--- quality/src/main/groovy/net/twisterrob/gradle/quality/QualityPlugin.groovy
+++ quality/src/main/groovy/net/twisterrob/gradle/quality/QualityPlugin.groovy
@@ -2,6 +2,7 @@ package net.twisterrob.gradle.quality
 
 import net.twisterrob.gradle.checkstyle.CheckStylePlugin
 import net.twisterrob.gradle.common.BaseExposedPlugin
+import net.twisterrob.gradle.pmd.PmdPlugin
 import org.gradle.api.Project
 
 class QualityPlugin extends BaseExposedPlugin {
@@ -11,5 +12,6 @@ class QualityPlugin extends BaseExposedPlugin {
 		super.apply(target)
 
 		project.apply plugin: CheckStylePlugin
+		project.apply plugin: PmdPlugin
 	}
 }
