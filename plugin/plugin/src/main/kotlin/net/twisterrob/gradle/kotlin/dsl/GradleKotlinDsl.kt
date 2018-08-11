b43dec257bb1f783d77017d939ec478ec9969a1a
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-08-11 23:59:10 +0000

    [SVN] r2608 Gradle Plugin in /Libraries/twister-plugin-gradle/ [ADD] withId wrapper for typed access

--- plugin/plugin/src/main/kotlin/net/twisterrob/gradle/kotlin/dsl/GradleKotlinDsl.kt
+++ plugin/plugin/src/main/kotlin/net/twisterrob/gradle/kotlin/dsl/GradleKotlinDsl.kt
@@ -1,8 +1,10 @@
 package net.twisterrob.gradle.kotlin.dsl
 
 import net.twisterrob.gradle.Utils
+import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.BasePluginConvention
+import org.gradle.api.plugins.PluginContainer
 import org.gradle.kotlin.dsl.getPluginByName
 
 /**
@@ -12,3 +14,17 @@ import org.gradle.kotlin.dsl.getPluginByName
 internal val Any.extensions get() = Utils.getExtensions(this)
 
 internal val Project.base: BasePluginConvention get() = convention.getPluginByName("base")
+
+/**
+ * Configures the [Plugin] with the given [pluginId] using [action], similar to [PluginContainer.withId], but type safe.
+ *
+ * @param P The assumed type of [Plugin].
+ * @param pluginId The ID of the [Plugin] to find.
+ * @param action The action to execute for the plugin (restrictions apply).
+ * @see [org.gradle.kotlin.dsl.withType] for a similar implementation
+ * @see [PluginContainer.withId] for the wrapped method
+ */
+inline fun <reified P : Plugin<*>> PluginContainer.withId(pluginId: String, crossinline action: P.() -> Unit) =
+	withId(pluginId) {
+		(it as P).action()
+	}
