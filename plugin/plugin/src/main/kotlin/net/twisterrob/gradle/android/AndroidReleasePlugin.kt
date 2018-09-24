6e0f7e134ea9c10d956f3f5fb821107bb3fa0cd3
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-09-24 22:44:49 +0000

    [SVN] r2663 Gradle Plugin in /Libraries/twister-plugin-gradle/ [REF] Konvert Release plugin to Kotlin

---
+++ plugin/plugin/src/main/kotlin/net/twisterrob/gradle/android/AndroidReleasePlugin.kt
@@ -0,0 +1,131 @@
+package net.twisterrob.gradle.android
+
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.LibraryPlugin
+import com.android.build.gradle.api.ApkVariant
+import com.android.build.gradle.internal.api.TestedVariant
+import com.android.builder.model.BuildType
+import net.twisterrob.gradle.Utils
+import net.twisterrob.gradle.base.BasePluginForKotlin
+import net.twisterrob.gradle.kotlin.dsl.extensions
+import org.gradle.api.DomainObjectSet
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.tasks.StopExecutionException
+import org.gradle.api.tasks.bundling.Zip
+import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.getByName
+import org.gradle.kotlin.dsl.withType
+import java.io.File
+
+private const val envVarName = "RELEASE_HOME"
+
+class AndroidReleasePlugin : BasePluginForKotlin() {
+	lateinit var android: BaseExtension
+
+	override fun apply(target: Project) {
+		super.apply(target)
+
+		android = project.extensions.getByName<BaseExtension>("android")
+
+		val allTask = project.tasks.create<Task>("release") {
+			group = org.gradle.api.plugins.BasePlugin.UPLOAD_GROUP
+			description = "Assembles and archives all builds"
+		}
+		project.afterEvaluate {
+			android.buildTypes.forEach { buildType ->
+				allTask.dependsOn(createReleaseTasks(buildType))
+			}
+		}
+	}
+
+	private fun createReleaseTasks(buildType: BuildType): Task {
+		val allBuildTypeTask = project.tasks.create<Task>("releaseAll${buildType.name.capitalize()}") {
+			group = org.gradle.api.plugins.BasePlugin.UPLOAD_GROUP
+			description = "Assembles and archives all ${buildType.name} builds"
+		}
+		LOG.debug("Creating tasks for {}", buildType.name)
+		fun createReleaseTaskWithDependency(variant: ApkVariant) {
+			allBuildTypeTask.dependsOn(createReleaseTask(variant))
+		}
+
+		@Suppress("UNCHECKED_CAST")
+		fun getVariantsForBuildType() =
+			Utils.getVariants(android).matching { it.buildType.name == buildType.name }
+					as DomainObjectSet<ApkVariant>
+
+		project.plugins.withType<AppPlugin> {
+			val matching = getVariantsForBuildType()
+			LOG.debug("Found android app, variants with {}: {}", buildType.name, matching)
+			matching.all(::createReleaseTaskWithDependency)
+		}
+		project.plugins.withType<LibraryPlugin> {
+			val matching = getVariantsForBuildType()
+			LOG.debug("Found android lib, variants with {}: {}", buildType.name, matching)
+			matching.all(::createReleaseTaskWithDependency)
+		}
+		return allBuildTypeTask
+	}
+
+	private fun createReleaseTask(variant: ApkVariant): Task {
+		val releaseVariantTask = project.tasks.create<Zip>("release${variant.name.capitalize()}") {
+			group = org.gradle.api.plugins.BasePlugin.UPLOAD_GROUP
+			description = "Assembles and archives apk and its proguard mapping for ${variant.description}"
+			val releaseDir = System.getenv(envVarName)
+				?: throw IllegalArgumentException("Please set ${envVarName} environment variable to a directory.")
+			destinationDir = File(releaseDir, "android")
+			archiveName = android.defaultConfig
+				.extensions.getByName<AndroidVersionExtension>("version")
+				.formatArtifactName(project, variant, "archive") + ".zip"
+			variant.outputs.forEach { output ->
+				from(output.outputFile)
+			}
+			if (variant is TestedVariant && variant.testVariant != null) {
+				variant.testVariant.outputs.forEach { output ->
+					from(output.outputFile)
+				}
+			}
+			if (variant.buildType.isMinifyEnabled) {
+				from(variant.mappingFile.parentFile) {
+					it.include("*")
+					it.rename("(.*)", "proguard_$1")
+				}
+			}
+			val outFile = outputs.files.singleFile
+			doFirst {
+				if (outFile.exists()) {
+					throw StopExecutionException("Target zip file already exists, did you run 'svn update'?\nRelease archive: ${outFile}")
+				}
+			}
+			doLast {
+				println("Published release artifacts to ${outFile}")
+			}
+		}
+		releaseVariantTask.dependsOn(variant.assemble)
+		if (variant is TestedVariant && variant.testVariant != null) {
+			releaseVariantTask.dependsOn(variant.testVariant.assemble)
+		}
+
+		variant.productFlavors.forEach { flavor ->
+			val releaseFlavorTaskName = "release${flavor.name.capitalize()}"
+			var releaseFlavorTask = project.tasks.findByName(releaseFlavorTaskName)
+			if (releaseFlavorTask == null) {
+				releaseFlavorTask = project.tasks.create<Task>(releaseFlavorTaskName) {
+					group = org.gradle.api.plugins.BasePlugin.UPLOAD_GROUP
+					description = "Assembles and archives all builds for flavor ${flavor.name}"
+				}
+			}
+			releaseFlavorTask.dependsOn(releaseVariantTask)
+		}
+		return releaseVariantTask
+		/*val task: AndroidReleaseTask = variant.install.project.tasks.create(
+			mapOf(
+				"name" to "release${variant.name.capitalize()}",
+				"type" to AndroidReleaserTask::class.java,
+				"dependsOn" to variant.assemble
+			)
+		)
+		task.variant = variant*/
+	}
+}
