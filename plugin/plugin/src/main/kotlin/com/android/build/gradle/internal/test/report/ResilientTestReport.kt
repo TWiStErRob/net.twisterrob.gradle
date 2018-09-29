2c5d3acd5efb17d9e6f8fbca3fb6b1a515b31592
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-09-29 20:24:10 +0000

    [SVN] r2670 Gradle Plugin in /Libraries/twister-plugin-gradle/ [REF] Konvert test reporting class to Kotlin

---
+++ plugin/plugin/src/main/kotlin/com/android/build/gradle/internal/test/report/ResilientTestReport.kt
@@ -0,0 +1,58 @@
+package com.android.build.gradle.internal.test.report
+
+import java.io.File
+
+/**
+ * This class is the bridge between the public world and AGP's internals.
+ */
+class ResilientTestReport(
+	reportType: ReportType,
+	resultDir: File,
+	reportDir: File
+) : TestReport(reportType, resultDir, reportDir) {
+
+	override fun generateReport() {
+		val model = loadModel()
+		val allClassResults = model.packages.flatMap { it.classes }
+		allClassResults.forEach(::polyfillResults)
+		generateFiles(model)
+	}
+
+	private fun polyfillResults(classResults: ClassTestResults) {
+		val results = classResults.testResultsMap
+		val template = results.values.first().values.first()
+		for (testName in results.flatMap { it.value.keys }) {
+			for (device in results.keys) {
+				if (!results.getValue(device).containsKey(testName)) {
+					val test = classResults.addTest(
+						testName, 0, device, template.project, template.flavor
+					)
+					test.ignored()
+					//test.addFailure("Missing run data", "N/A", device, template.project, template.flavor)
+				}
+			}
+		}
+	}
+
+	/**
+	 * Internal AGP API not designed for extensibility, so need to hack around a bit.
+	 */
+	companion object {
+
+		/**
+		 * @see TestReport.loadModel
+		 */
+		private fun TestReport.loadModel() =
+			this::class.java.getDeclaredMethod("loadModel")
+				.apply { isAccessible = true }
+				.invoke(this) as AllTestResults
+
+		/**
+		 * @see TestReport.generateFiles
+		 */
+		private fun TestReport.generateFiles(model: AllTestResults) =
+			this::class.java.getDeclaredMethod("generateFiles")
+				.apply { isAccessible = true }
+				.invoke(this, model) as Unit
+	}
+}
