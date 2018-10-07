edd3929d05aace30e49ba4465156b5aae8bdd1a6
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-10-07 13:59:32 +0100

    Remove @Input to allow users to give real closures capturing locals

--- quality/src/main/kotlin/net/twisterrob/gradle/quality/tasks/ValidateViolationsTask.kt
+++ quality/src/main/kotlin/net/twisterrob/gradle/quality/tasks/ValidateViolationsTask.kt
@@ -17,14 +17,12 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.plugins.JavaBasePlugin
-import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 import se.bjurr.violations.lib.model.SEVERITY
 import se.bjurr.violations.lib.reports.Parser
 
 open class ValidateViolationsTask : DefaultTask() {
 
-	@Input
 	@Suppress("MemberVisibilityCanBePrivate") // DSL
 	var action: Action<Grouper.Start<Violations>> = Action(::defaultAction)
 
