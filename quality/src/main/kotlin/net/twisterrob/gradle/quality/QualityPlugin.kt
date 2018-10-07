8d281484decb74d20f1eea13a1556ff4cee93d3d
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-10-07 23:29:32 +0100

    Apply usual tasks with known names

--- quality/src/main/kotlin/net/twisterrob/gradle/quality/QualityPlugin.kt
+++ quality/src/main/kotlin/net/twisterrob/gradle/quality/QualityPlugin.kt
@@ -3,6 +3,9 @@ package net.twisterrob.gradle.quality
 import net.twisterrob.gradle.checkstyle.CheckStylePlugin
 import net.twisterrob.gradle.common.BaseExposedPlugin
 import net.twisterrob.gradle.pmd.PmdPlugin
+import net.twisterrob.gradle.quality.tasks.GlobalLintGlobalFinalizerTask
+import net.twisterrob.gradle.quality.tasks.HtmlReportTask
+import net.twisterrob.gradle.quality.tasks.ValidateViolationsTask
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 
@@ -15,5 +18,13 @@ class QualityPlugin : BaseExposedPlugin() {
 		project.plugins.apply("org.gradle.reporting-base")
 		project.apply<CheckStylePlugin>()
 		project.apply<PmdPlugin>()
+
+		project.tasks.register("violationReportConsole", ValidateViolationsTask::class.java)
+		project.tasks.register("violationReportHtml", HtmlReportTask::class.java)
+		project.afterEvaluate {
+			if (project.tasks.findByName("lint") == null) {
+				project.tasks.register("lint", GlobalLintGlobalFinalizerTask::class.java)
+			}
+		}
 	}
 }
