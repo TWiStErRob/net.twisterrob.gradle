75ede2946d8d99c7fe50271ff98d2803d1c2f85b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-10-08 20:00:06 +0100

    Fix #16: TableGenerator empty data handling (#17)

--- quality/src/test/kotlin/net/twisterrob/gradle/quality/report/TableGeneratorTest.kt
+++ quality/src/test/kotlin/net/twisterrob/gradle/quality/report/TableGeneratorTest.kt
@@ -43,7 +43,7 @@ class TableGeneratorTest {
 			module2	variant2 	       N/A	N/A	      N/A
 			module2	variant3 	         0	N/A	      N/A
 			module3	variant2 	       N/A	N/A	      N/A
-			Summary	(total 6)	         1	  5	      N/A
+			Summary	(total: 6)	         1	  5	      N/A
 		""".prepare()
 		assertEquals(expected, result)
 	}
@@ -58,7 +58,7 @@ class TableGeneratorTest {
 			module1	variant1 	         1	  2	      N/A
 			module1	variant2 	       N/A	  3	      N/A
 			module2	variant3 	         0	N/A	      N/A
-			Summary	(total 6)	         1	  5	      N/A
+			Summary	(total: 6)	         1	  5	      N/A
 		""".prepare()
 		assertEquals(expected, result)
 	}
@@ -75,7 +75,7 @@ class TableGeneratorTest {
 			module2	variant2 	       N/A	N/A
 			module2	variant3 	         0	N/A
 			module3	variant2 	       N/A	N/A
-			Summary	(total 6)	         1	  5
+			Summary	(total: 6)	         1	  5
 		""".prepare()
 		assertEquals(expected, result)
 	}
@@ -90,7 +90,58 @@ class TableGeneratorTest {
 			module1	variant1 	         1	  2
 			module1	variant2 	       N/A	  3
 			module2	variant3 	         0	N/A
-			Summary	(total 6)	         1	  5
+			Summary	(total: 6)	         1	  5
+		""".prepare()
+		assertEquals(expected, result)
+	}
+
+	@Test fun `empty data (no modules)`() {
+		val sut = TableGenerator()
+
+		val result = sut.build(emptyMap())
+
+		val expected = """
+			module 	variant${"   "}
+			Summary	(total: 0)
+		""".prepare()
+		assertEquals(expected, result)
+	}
+
+	@Test fun `empty data (no variants)`() {
+		val sut = TableGenerator()
+
+		val result = sut.build(
+			mapOf(
+				"module1" to emptyMap(),
+				"module2" to emptyMap(),
+				"module3" to emptyMap()
+			)
+		)
+
+		val expected = """
+			module 	variant${"   "}
+			Summary	(total: 0)
+		""".prepare()
+		assertEquals(expected, result)
+	}
+
+	@Test fun `empty data (no parsers)`() {
+		val sut = TableGenerator()
+
+		val result = sut.build(
+			mapOf(
+				"module1" to mapOf("variant1" to emptyMap()),
+				"module2" to mapOf("variant2" to emptyMap()),
+				"module3" to mapOf("variant3" to emptyMap())
+			)
+		)
+
+		val expected = """
+			module 	variant${"   "}
+			module1	variant1${"  "}
+			module2	variant2${"  "}
+			module3	variant3${"  "}
+			Summary	(total: 0)
 		""".prepare()
 		assertEquals(expected, result)
 	}
