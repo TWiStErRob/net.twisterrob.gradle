f439690e7c97701a784b511d495fe065bdb6a7c1
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-10-09 22:10:09 +0100

    Fixes #18: :lint task needs to configure other lint tasks (#19)
    * Add source code for Android LINT to be able to debug
    * Fix #18 by making sure applyAfterPluginConfigured runs code always even after afterEvaluate has finished

--- quality/src/test/kotlin/net/twisterrob/gradle/quality/tasks/GlobalLintGlobalFinalizerTaskTest.kt
+++ quality/src/test/kotlin/net/twisterrob/gradle/quality/tasks/GlobalLintGlobalFinalizerTaskTest.kt
@@ -133,6 +133,70 @@ class GlobalLintGlobalFinalizerTaskTest {
 		result.assertHasOutputLine("> Ran lint on subprojects: ${1 + 1 + 1} issues found".toRegex())
 	}
 
+
+	@Test fun `gathers results from submodules (lazy init)`() {
+		`given`@
+		@Language("java")
+		val lintViolation = """
+			class LintFailure {
+				void f() {
+					android.util.Log.e("123456789012345678901234", "");
+				}
+			}
+		""".trimIndent()
+		val modules = arrayOf(
+			"module1",
+			"module2",
+			"module3"
+		)
+		modules.forEach { module ->
+			@Language("gradle")
+			val subProject = """
+				apply plugin: 'com.android.library'
+				android {
+					lintOptions {
+						check 'LongLogTag'
+					}
+				}
+			""".trimIndent()
+
+			@Language("xml")
+			val manifest = """
+				<manifest package="project.${module}" />
+			""".trimIndent()
+
+			gradle.file(subProject, module, "build.gradle")
+			gradle.settingsFile().appendText("include ':${module}'${endl}")
+			gradle.file(lintViolation, module, "src", "main", "java", "fail1.java")
+			gradle.file(manifest, module, "src", "main", "AndroidManifest.xml")
+		}
+
+		@Language("gradle")
+		val script = """
+			subprojects {
+				repositories { google() } // needed for com.android.tools.lint:lint-gradle resolution
+			}
+			tasks.register('lint', ${GlobalLintGlobalFinalizerTask::class.java.name})
+		""".trimIndent()
+
+		val result: BuildResult
+		`when`@
+		result = gradle
+			.basedOn("android-multi_module")
+			.run(script, "lint")
+			.buildAndFail()
+
+		//`then`@
+		val lintTasks = result.tasks.map { it.path }.filter { it.endsWith(":lint") }
+		assertThat(lintTasks, hasItems(":module1:lint", ":module2:lint", ":module3:lint"))
+		assertThat(lintTasks.last(), equalTo(":lint"))
+		assertEquals(TaskOutcome.SUCCESS, result.task(":module1:lint")!!.outcome)
+		assertEquals(TaskOutcome.SUCCESS, result.task(":module2:lint")!!.outcome)
+		assertEquals(TaskOutcome.SUCCESS, result.task(":module3:lint")!!.outcome)
+		assertEquals(TaskOutcome.FAILED, result.task(":lint")!!.outcome)
+		result.assertHasOutputLine("> Ran lint on subprojects: ${1 + 1 + 1} issues found".toRegex())
+	}
+
 	companion object {
 
 		private val endl = System.lineSeparator()
