5694ceba893e8a1e7b8290737ad8e856c1558905
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-10-08 23:10:59 +0100

    Add error handling on sure operators to get more info

--- plugin/plugin/src/main/kotlin/net/twisterrob/gradle/Global.kt
+++ plugin/plugin/src/main/kotlin/net/twisterrob/gradle/Global.kt
@@ -13,12 +13,14 @@ import kotlin.reflect.KProperty
 
 val builtDate: Instant by lazy {
 	val aClassInJar = BasePlugin::class.java
-	val res = aClassInJar.getResource(aClassInJar.simpleName + ".class")!!
-	val url = res.openConnection()!!
+	val aClassName = aClassInJar.simpleName + ".class"
+	val res = aClassInJar.getResource(aClassName) ?: error("Cannot find class file ${aClassName}")
+	val url = res.openConnection() ?: error("Cannot open ${res}")
 	when {
 		url is JarURLConnection -> {
-			val mf = url.manifest!!
-			val date = mf.mainAttributes.getValue("Built-Date")!!
+			val mf = url.manifest ?: error("Cannot find manifest in ${url.jarFileURL}")
+			val date = mf.mainAttributes.getValue("Built-Date")
+				?: error("Build-Date attribute not present in manifest\n${url.manifest.mainAttributes.toMap()}")
 			Instant.from(DateTimeFormatter.ISO_INSTANT.parse(date))
 		}
 
