656c8096d10b7f57f406c912414ff74584d4304c
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-10-16 21:49:22 +0100

    New Version type

---
+++ plugin/plugin/src/test/kotlin/net/twisterrob/gradle/test/AGPVersion.kt
@@ -0,0 +1,56 @@
+package net.twisterrob.gradle.test
+
+data class AGPVersion(
+	val major: Int,
+	val minor: Int?,
+	val type: ReleaseType?,
+	val patch: Int?,
+) : Comparable<AGPVersion> {
+
+	init {
+		if (patch != null) checkNotNull(type) { "If a patch release is given, you must specify its type." }
+		if (patch != null) checkNotNull(minor) { "Cannot specify a patch version without minor." }
+		if (type != null) checkNotNull(minor) { "Cannot specify a release type without minor." }
+	}
+
+	enum class ReleaseType : Comparable<ReleaseType> {
+		Alpha,
+		Beta,
+		Candidate,
+		Stable,
+	}
+
+	override fun toString(): String =
+		listOfNotNull(major, minor, type, patch).joinToString(separator = ".")
+
+	companion object {
+		val AGP_VERSION_UNDER_TEST: AGPVersion
+			get() = ofAGP(System.getProperty("net.twisterrob.test.android.pluginVersion"))
+
+		private val AGP_VERSION_REGEX =
+			"""^(?<major>\d+)(?:\.(?<minor>\d+)(?:\.(?<patch>\d+))?(?:-(?<type>alpha|beta|rc)(?<iteration>\d+))?)?$""".toRegex()
+
+		fun ofAGP(version: String): AGPVersion {
+			val match = AGP_VERSION_REGEX.matchEntire(version)
+				?: error("Unrecognised Android Gradle Plugin version: ${version}, only ${AGP_VERSION_REGEX} are supported.")
+			val major = match.groups["major"]!!.value.toInt()
+			val minor = match.groups["minor"]?.value?.toInt()
+			val patch = match.groups["patch"]?.value?.toInt()
+			val iteration = match.groups["iteration"]?.value?.trimStart('0')?.toInt()
+			if (iteration != null && patch != 0) {
+				error("Invalid version, pattern: <major>.<minor>.<patch>-<type><iteration> is not supported with patch other than 0.")
+			}
+			val type = when (val preReleaseTypeString = match.groups["type"]?.value) {
+				null -> ReleaseType.Stable
+				"alpha" -> ReleaseType.Alpha
+				"beta" -> ReleaseType.Beta
+				"rc" -> ReleaseType.Candidate
+				else -> error("Unknown ${preReleaseTypeString}")
+			}
+			return AGPVersion(major, minor, type, iteration ?: patch)
+		}
+	}
+
+	override fun compareTo(other: AGPVersion): Int =
+		compareValuesBy(this, other, AGPVersion::major, AGPVersion::minor, AGPVersion::type, AGPVersion::patch)
+}
