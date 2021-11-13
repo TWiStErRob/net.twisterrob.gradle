4358d7cac434d6c58d1b6c5a92aa6d31d4e7a979
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-11-13 21:32:40 +0000

    Remove unused setting.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.util.GradleVersion
-
 rootProject.name = "net.twisterrob.gradle"
 
 include(":quality")
@@ -11,13 +9,6 @@ include(":test:internal")
 listOf("checkstyle", "pmd").forEach {
 	include(":${it}")
 	project(":${it}").projectDir = file("checkers/${it}")
-}
-
-// As part of making the publishing plugins stable,
-// the 'deferred configurable' behavior of the 'publishing {}' block is now deprecated.
-// https://docs.gradle.org/4.8/userguide/publishing_maven.html#publishing_maven:deferred_configuration.
-if (GradleVersion.current().baseVersion < GradleVersion.version("5.0")) {
-	enableFeaturePreview("STABLE_PUBLISHING")
 }
 
 if (settings.extra["net.twisterrob.gradle.build.includeExamples"].toString().toBoolean()) {
