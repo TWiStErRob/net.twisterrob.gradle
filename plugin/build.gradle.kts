c7a751ed3a1e138169ea5e83e1f5f03ab36fb666
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-11-20 13:29:53 +0000

    Extract reusable build configuration

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -65,22 +65,7 @@ allprojects {
 
 	afterEvaluate {
 		try {
-			tasks.named<PluginUnderTestMetadata>("pluginUnderTestMetadata") {
-				// In Gradle 6.5.1 to 6.6 upgrade something changed.
-				// The folders on the classpath
-				// classpath files('net.twisterrob.gradle\\plugin\\plugin\\build\\classes\\java\\main')
-				// classpath files('net.twisterrob.gradle\\plugin\\plugin\\build\\classes\\kotlin\\main')
-				// classpath files('net.twisterrob.gradle\\plugin\\plugin\\build\\resources\\main')
-				// are now used as a quickly ZIPped JAR file
-				// file:/Temp/.gradle-test-kit-TWiStEr-6/caches/jars-8/612d2cded1e3015b824ce72a63bd2fb6/main.jar
-				// but this is missing the MANIFEST.MF file, as only the class and resource files are there.
-				// Adding the temporary directory for the manifest is not enough like this:
-				// it.pluginClasspath.from(files(file("build/tmp/jar/")))
-				// because it needs to be in the same JAR file as the class files.
-				// To work around this: prepend the final JAR file on the classpath:
-				val jar = tasks.named<Jar>("jar").get()
-				pluginClasspath.setFrom(files(jar.archiveFile) + pluginClasspath)
-			}
+			addJarToClasspathOfPlugin()
 		} catch (ex: UnknownTaskException) {
 			if (ex.message?.startsWith("Task with name 'pluginUnderTestMetadata' not found in project ") == true) {
 				// All good, ignore.
