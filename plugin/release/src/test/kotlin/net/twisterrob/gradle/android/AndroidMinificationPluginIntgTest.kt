4b36caeb66694002c1778c07200a25c022a4e458
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-11-21 12:20:16 +0000

    Fix expectation for R8 in AGP 4.0.3

--- plugin/release/src/test/kotlin/net/twisterrob/gradle/android/AndroidMinificationPluginIntgTest.kt
+++ plugin/release/src/test/kotlin/net/twisterrob/gradle/android/AndroidMinificationPluginIntgTest.kt
@@ -177,13 +177,13 @@ class AndroidMinificationPluginIntgTest : BaseAndroidIntgTest() {
 
 			Minification.R8 -> {
 				// Not supported on R8 at AGP 4.2.
-				result.assertNoOutputLine(""".*WARNING:.* R8: Ignoring option: -dump.*""".toRegex())
+				result.assertNoOutputLine(""".*R8: Ignoring option: -dump.*""".toRegex())
 				assertThat(gradle.root.resolve("build/outputs/mapping/release/dump.txt"), not(anExistingFile()))
 			}
 
 			Minification.R8Full -> {
 				// Not supported on R8 at AGP 4.2.
-				result.assertNoOutputLine(""".*WARNING:.* R8: Ignoring option: -dump.*""".toRegex())
+				result.assertNoOutputLine(""".*R8: Ignoring option: -dump.*""".toRegex())
 				assertThat(gradle.root.resolve("build/outputs/mapping/release/dump.txt"), not(anExistingFile()))
 			}
 		}
