e3b1c6b179aa602815cd6eac4a7d9acdf5f8923e
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-11-21 11:52:04 +0000

    Move GradlePlugin application to root plugins.

--- plugin/base/src/main/kotlin/net/twisterrob/gradle/root/RootPlugin.kt
+++ plugin/base/src/main/kotlin/net/twisterrob/gradle/root/RootPlugin.kt
@@ -1,5 +1,14 @@
 package net.twisterrob.gradle.root
 
 import net.twisterrob.gradle.base.BaseExposedPlugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
 
-class RootPlugin : BaseExposedPlugin()
+class RootPlugin : BaseExposedPlugin() {
+
+	override fun apply(target: Project) {
+		super.apply(target)
+
+		project.apply<GradlePlugin>()
+	}
+}
