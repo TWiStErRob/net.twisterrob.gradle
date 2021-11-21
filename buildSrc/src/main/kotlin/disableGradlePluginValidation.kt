df1f9e61373bddb1a5016cb1a9ef6ab8888d6626
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-11-21 13:31:53 +0000

    Document plugin validation hack

--- buildSrc/src/main/kotlin/disableGradlePluginValidation.kt
+++ buildSrc/src/main/kotlin/disableGradlePluginValidation.kt
@@ -2,6 +2,13 @@ import org.gradle.api.Project
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.kotlin.dsl.named
 
+/**
+ * Disables this warning when `java-gradle-plugin` is applied, but there are no `gradlePlugin.plugins` created.
+ * ```
+ * > Task :foo:bar:jar
+ * :foo:bar:jar: No valid plugin descriptors were found in META-INF/gradle-plugins
+ * ```
+ */
 fun Project.disableGradlePluginValidation() {
 	tasks.named<Jar>("jar") {
 		@Suppress("LocalVariableName")
