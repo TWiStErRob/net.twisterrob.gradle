1805f81fc8177c2edb2ee86283b1e159a6d9d865
Róbert Papp <papp.robert.s@gmail.com>
2021-11-21 13:58:39 +0000

    Fix plugin validation hack (#182)
    * Fix disableGradlePluginValidation to actually remove check
    * Double-self-check that it's valid to remove validation.

--- buildSrc/src/main/kotlin/disableGradlePluginValidation.kt
+++ buildSrc/src/main/kotlin/disableGradlePluginValidation.kt
@@ -1,12 +1,35 @@
+import org.gradle.api.GradleScriptException
 import org.gradle.api.Project
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.kotlin.dsl.named
 
 fun Project.disableGradlePluginValidation() {
+	check(gradlePlugin.plugins.isEmpty()) { "There are plugins declared in ${this}, don't disable plugin validation." }
+
+	@Suppress("LocalVariableName")
 	tasks.named<Jar>("jar") {
-		@Suppress("LocalVariableName")
 		val PluginValidationAction =
 			Class.forName("org.gradle.plugin.devel.plugins.JavaGradlePluginPlugin\$PluginValidationAction")
-		taskActions.removeIf { PluginValidationAction.isInstance(it) }
+		val TaskActionWrapper =
+			Class.forName("org.gradle.api.internal.AbstractTask\$TaskActionWrapper")
+		val actionField = TaskActionWrapper.getDeclaredField("action").apply { isAccessible = true }
+
+		val removed = taskActions.removeIf {
+			TaskActionWrapper.isInstance(it) && PluginValidationAction.isInstance(actionField.get(it))
+		}
+		if (!removed) {
+			val actions = taskActions
+				.map {
+					when {
+						TaskActionWrapper.isInstance(it) -> "${it} -> ${actionField.get(it)}"
+						else -> it
+					}
+				}
+				.joinToString(prefix = System.lineSeparator(), separator = System.lineSeparator())
+			throw GradleScriptException(
+				"Tried to remove ${PluginValidationAction} from ${this@named}, but it didn't happen.",
+				Throwable("Actions:${actions}") // Satisfy compiler, even though cause can be null.
+			)
+		}
 	}
 }
