6425d51f1cf2451159789a2abd44e8e09f7dac66
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-11-21 12:11:13 +0000

    Document hack

--- plugin/compat-40/src/main/kotlin/net/twisterrob/gradle/internal/android/AndroidHelpers-addBuildConfigField40x.kt
+++ plugin/compat-40/src/main/kotlin/net/twisterrob/gradle/internal/android/AndroidHelpers-addBuildConfigField40x.kt
@@ -8,5 +8,6 @@ import java.io.Serializable
 
 fun Project.addBuildConfigField40x(name: String, type: String, value: Provider<out Serializable>) {
 	val android: BaseExtension = this.extensions.getByName<BaseExtension>("android")
+	// Note: this .get() is supported by a conditional `task.writeVCS()` in CalculateVCSRevisionInfoTask.
 	android.defaultConfig.buildConfigField(type, name, value.get().toString())
 }
