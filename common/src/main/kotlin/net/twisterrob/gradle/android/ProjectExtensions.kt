7daa4ce0feab4b80c8f7eb1a2095fdb0c1260cc5
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-11-30 23:26:42 +0000

    Remove overly strict restriction on androidComponents

--- common/src/main/kotlin/net/twisterrob/gradle/android/ProjectExtensions.kt
+++ common/src/main/kotlin/net/twisterrob/gradle/android/ProjectExtensions.kt
@@ -1,6 +1,7 @@
 package net.twisterrob.gradle.android
 
 import com.android.build.api.variant.AndroidComponentsExtension
+import com.android.build.gradle.api.AndroidBasePlugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.PluginInstantiationException
 import org.gradle.kotlin.dsl.get
@@ -10,7 +11,9 @@ import org.gradle.kotlin.dsl.get
  */
 val Project.androidComponents: AndroidComponentsExtension<*, *, *>
 	get() {
-		if (!this.plugins.hasPlugin("com.android.base")) {
+		// REPORT hasPlugin("com.android.base") should be equivalent, but returns false during plugins.withType<ABP> { }
+		// because com.android.build.gradle.internal.plugins.BasePlugin applies ABP class not ID?
+		if (!this.plugins.hasPlugin(AndroidBasePlugin::class.java)) {
 			throw PluginInstantiationException("Cannot use this before the Android plugins are applied.")
 		}
 		return this.extensions["androidComponents"] as AndroidComponentsExtension<*, *, *>
