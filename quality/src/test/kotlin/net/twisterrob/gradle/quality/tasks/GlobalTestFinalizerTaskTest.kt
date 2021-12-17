b73db5345f22e5bfd84988800528bc159ac3e4e8
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-12-17 12:42:26 +0000

    Make sure testReport does not execute tests

--- quality/src/test/kotlin/net/twisterrob/gradle/quality/tasks/GlobalTestFinalizerTaskTest.kt
+++ quality/src/test/kotlin/net/twisterrob/gradle/quality/tasks/GlobalTestFinalizerTaskTest.kt
@@ -9,6 +9,8 @@ import net.twisterrob.gradle.test.assertNoOutputLine
 import net.twisterrob.gradle.test.runBuild
 import net.twisterrob.gradle.test.runFailingBuild
 import org.gradle.testkit.runner.TaskOutcome
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.hasSize
 import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -65,6 +67,28 @@ class GlobalTestFinalizerTaskTest : BaseIntgTest() {
 
 		assertEquals(TaskOutcome.UP_TO_DATE, result.task(":test")!!.outcome)
 		assertEquals(TaskOutcome.SUCCESS, result.task(":testReport")!!.outcome)
+		result.assertNoOutputLine(Regex(""".*failing tests.*"""))
+		result.assertNoOutputLine(Regex(""".*See the report at.*"""))
+	}
+
+	@Test fun `does not execute or compile tests`() {
+		gradle.file(testFile, "src", "test", "java", "Tests.java")
+
+		@Language("gradle")
+		val script = """
+			apply plugin: 'net.twisterrob.quality'
+			dependencies {
+				testImplementation 'junit:junit:${Version.id()}'
+			}
+		""".trimIndent()
+
+		val result = gradle.runBuild {
+			basedOn("android-root_app")
+			run(script, "testReport")
+		}
+
+		assertEquals(TaskOutcome.SUCCESS, result.task(":testReport")!!.outcome)
+		assertThat(result.tasks, hasSize(1))
 		result.assertNoOutputLine(Regex(""".*failing tests.*"""))
 		result.assertNoOutputLine(Regex(""".*See the report at.*"""))
 	}
