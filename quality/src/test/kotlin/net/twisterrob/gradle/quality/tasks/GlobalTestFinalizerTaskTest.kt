5898219671eb42d3f42931b3a8d12ea0f8a1421f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-12-17 12:54:09 +0000

    Test test execution on GlobalTestFinalizerTask

--- quality/src/test/kotlin/net/twisterrob/gradle/quality/tasks/GlobalTestFinalizerTaskTest.kt
+++ quality/src/test/kotlin/net/twisterrob/gradle/quality/tasks/GlobalTestFinalizerTaskTest.kt
@@ -6,10 +6,13 @@ import net.twisterrob.gradle.test.GradleRunnerRule
 import net.twisterrob.gradle.test.GradleRunnerRuleExtension
 import net.twisterrob.gradle.test.assertHasOutputLine
 import net.twisterrob.gradle.test.assertNoOutputLine
+import net.twisterrob.gradle.test.assertNoTask
 import net.twisterrob.gradle.test.runBuild
 import net.twisterrob.gradle.test.runFailingBuild
 import org.gradle.testkit.runner.TaskOutcome
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.contains
+import org.hamcrest.Matchers.either
 import org.hamcrest.Matchers.hasSize
 import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Test
@@ -110,5 +113,50 @@ class GlobalTestFinalizerTaskTest : BaseIntgTest() {
 		assertEquals(TaskOutcome.SUCCESS, result.task(":test")!!.outcome)
 		assertEquals(TaskOutcome.FAILED, result.task(":testReport")!!.outcome)
 		result.assertHasOutputLine(Regex("""> There were ${3 + 3} failing tests. See the report at: .*"""))
+	}
+
+	@Test fun `test tasks fail without executing testReport when executing explicitly`() {
+		gradle.file(testFile, "src", "test", "java", "Tests.java")
+
+		@Language("gradle")
+		val script = """
+			apply plugin: 'net.twisterrob.quality'
+			dependencies {
+				testImplementation 'junit:junit:${Version.id()}'
+			}
+		""".trimIndent()
+
+		val result = gradle.runFailingBuild {
+			basedOn("android-root_app")
+			run(script, "testDebugUnitTest")
+		}
+
+		assertEquals(TaskOutcome.FAILED, result.task(":testDebugUnitTest")!!.outcome)
+		result.assertNoTask(":testReport")
+		result.assertHasOutputLine(Regex("""> There were failing tests. See the report at: .*"""))
+	}
+
+	@Test fun `test tasks fail without executing testReport when executing indirectly`() {
+		gradle.file(testFile, "src", "test", "java", "Tests.java")
+
+		@Language("gradle")
+		val script = """
+			apply plugin: 'net.twisterrob.quality'
+			dependencies {
+				testImplementation 'junit:junit:${Version.id()}'
+			}
+		""".trimIndent()
+
+		val result = gradle.runFailingBuild {
+			basedOn("android-root_app")
+			run(script, "test")
+		}
+
+		assertThat(
+			result.taskPaths(TaskOutcome.FAILED),
+			either(contains(":testDebugUnitTest")).or(contains(":testReleaseUnitTest"))
+		)
+		result.assertNoTask(":testReport")
+		result.assertHasOutputLine(Regex("""> There were failing tests. See the report at: .*"""))
 	}
 }
