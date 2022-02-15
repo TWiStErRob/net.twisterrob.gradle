b15c9ce7edbb83d69015215a8ed8d2aa9b62c580
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-02-15 20:59:12 +0000

    The name of version catalogs must end with 'Libs' to reduce chances of extension conflicts. This will fail with an error in Gradle 8.0. See https://docs.gradle.org/7.4/userguide/platforms.html#sec:multiple for more details.

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -2,7 +2,7 @@ dependencyResolutionManagement {
 	@Suppress("UnstableApiUsage")
 	versionCatalogs {
 		// TODEL use libs, see https://github.com/gradle/gradle/issues/18999 / https://youtrack.jetbrains.com/issue/KTIJ-20166
-		create("deps") {
+		create("projectLibs") {
 			from(files("../gradle/libs.versions.toml"))
 		}
 	}
