1cc73218592cebbec32c352fec9ebe60aaee88a2
Róbert Papp <papp.robert.s@gmail.com>
2022-04-02 19:07:29 +0100

    Fix #201: auto-use GIT hash for versionName (#222)

--- plugin/versioning/src/testFixtures/kotlin/net/twisterrob/gradle/vcs/VCSHelpers.kt
+++ plugin/versioning/src/testFixtures/kotlin/net/twisterrob/gradle/vcs/VCSHelpers.kt
@@ -1,11 +1,12 @@
 package net.twisterrob.gradle.vcs
 
 import java.io.File
+import kotlin.math.absoluteValue
 
 fun File.createTestFileToCommit(): File =
 	createTestFileToCommit(Math.random().toString())
 
 fun File.createTestFileToCommit(message: String): File {
-	val hash = message.hashCode().toString(16).padStart(8, '0')
-	return resolve("commit_$hash.commit").also { it.writeText(message) }
+	val hash = message.hashCode().absoluteValue.toString(16).padStart(8, '0')
+	return resolve("test_commit_$hash.txt").also { it.writeText(message) }
 }
