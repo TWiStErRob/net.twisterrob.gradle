ac431e7ef46c3639449358b3d0025cc1fb36b4ac
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-05-24 22:42:12 +0100

    Add more assertions to clarify intent

--- plugin/languages/src/test/kotlin/net/twisterrob/gradle/kotlin/KotlinPluginIntgTest.kt
+++ plugin/languages/src/test/kotlin/net/twisterrob/gradle/kotlin/KotlinPluginIntgTest.kt
@@ -6,6 +6,7 @@ import net.twisterrob.gradle.test.GradleBuildTestResources.basedOn
 import net.twisterrob.gradle.test.GradleRunnerRule
 import net.twisterrob.gradle.test.GradleRunnerRuleExtension
 import net.twisterrob.gradle.test.assertHasOutputLine
+import net.twisterrob.gradle.test.assertNoOutputLine
 import net.twisterrob.gradle.test.assertSuccess
 import net.twisterrob.test.compile.generateKotlinCompilationCheck
 import net.twisterrob.test.compile.generateKotlinCompilationCheckTest
@@ -101,6 +102,8 @@ class KotlinPluginIntgTest : BaseIntgTest() {
 
 		val result = gradle.run(script, "jar").buildAndFail()
 
+		result.assertNoOutputLine(""".*Build was configured to prefer settings repositories over project repositories but repository 'MavenRepo' was added by plugin 'net\.twisterrob\.kotlin'""".toRegex())
+		result.assertNoOutputLine(""".*Build was configured to prefer settings repositories over project repositories but repository '.*' was added by plugin '.*'""".toRegex())
 		result.assertHasOutputLine(""".*Cannot resolve external dependency (.*) because no repositories are defined\.""".toRegex())
 	}
 }
