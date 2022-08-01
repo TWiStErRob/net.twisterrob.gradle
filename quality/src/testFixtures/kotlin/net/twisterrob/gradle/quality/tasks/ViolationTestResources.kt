04b2438aa2851dfec6c47731e3b50e99d2be5df3
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-08-01 22:02:44 +0100

    Fix report output

---
+++ quality/src/testFixtures/kotlin/net/twisterrob/gradle/quality/tasks/ViolationTestResources.kt
@@ -0,0 +1,93 @@
+package net.twisterrob.gradle.quality.tasks
+
+import java.io.File
+
+class ViolationTestResources(
+	private val rootProject: File
+) {
+
+	val everything: Everything get() = Everything()
+
+	inner class Everything {
+		val checkstyleReport: String
+			get() = read("ViolationTestResources/everything/checkstyle-multiple.xml")
+				.replace("{{rootProject}}", rootProject.absolutePath)
+
+		val pmdReport: String
+			get() = read("ViolationTestResources/everything/pmd-multiple.xml")
+				.replace("{{rootProject}}", rootProject.absolutePath)
+
+		val lintReport: String
+			get() = read("ViolationTestResources/everything/lint-multiple.xml")
+				.replace("{{rootProject}}", rootProject.absolutePath)
+
+		/**
+		 * @warning DO NOT EDIT (https://youtrack.jetbrains.com/issue/IDEA-171699)
+		 * NOR COMMIT THIS FILE IN IDEA (https://youtrack.jetbrains.com/issue/IDEA-79736)
+		 */
+		@Suppress("MaxLineLength")
+		val violationsXml: String
+			get() = read("ViolationTestResources/everything/violations.xml")
+				// <violations project="..."
+				.replace(
+					"""(?<=project=")junit12252005098066695430(?=")""".toRegex(),
+					Regex.escapeReplacement(rootProject.name)
+				)
+				// <location file="..."
+				.replace(
+					"""(?<=")C:\\Users\\TWiStEr\\AppData\\Local\\Temp\\junit12252005098066695430((?:\\.+)+)(?=")""".toRegex(),
+					Regex.escapeReplacement(rootProject.absolutePath) + "$1"
+				)
+				// <location fileAbsoluteAsUrl="..."
+				.replace(
+					"""(?<=")file:/C:/Users/TWiStEr/AppData/Local/Temp/junit12252005098066695430((?:/.+)+)(?=")""".toRegex(),
+					Regex.escapeReplacement(rootProject.toURI().toString().removeSuffix("/")) + "$1"
+				)
+				// <violation[details/@rule="IconMissingDensityFolder"]/description
+				.replace(
+					"""(?<=\\`)C:\\\\Users\\\\TWiStEr\\\\AppData\\\\Local\\\\Temp\\\\junit12252005098066695430((?:\\\\.+)+)(?=\\`)""".toRegex(),
+					Regex.escapeReplacement(rootProject.absolutePath.replace("\\", "\\\\")) + "$1"
+				)
+
+		/**
+		 * @warning DO NOT EDIT (https://youtrack.jetbrains.com/issue/IDEA-171699)
+		 * NOR COMMIT THIS FILE IN IDEA (https://youtrack.jetbrains.com/issue/IDEA-79736)
+		 */
+		@Suppress("MaxLineLength")
+		val violationsHtml: String
+			get() = read("ViolationTestResources/everything/violations.html")
+				// <title>...
+				.replace(
+					"""(?<=<title>)junit12252005098066695430""".toRegex(),
+					Regex.escapeReplacement(rootProject.name)
+				)
+				// <h1 id="top">Violations report for ...</h1>
+				.replace(
+					"""junit12252005098066695430(?=</h1>)""".toRegex(),
+					Regex.escapeReplacement(rootProject.name)
+				)
+				// <script>render.markdown(`...\`...\`...`)</script>
+				.replace(
+					"""(?<=\\`)C:\\\\Users\\\\TWiStEr\\\\AppData\\\\Local\\\\Temp\\\\junit12252005098066695430((?:\\\\.+)+)(?=\\`)""".toRegex(),
+					Regex.escapeReplacement(rootProject.absolutePath.replace("\\", "\\\\")) + "$1"
+				)
+				// <a class="file" href="file:/..."
+				.replace(
+					"""(?<=")file:/C:/Users/TWiStEr/AppData/Local/Temp/junit12252005098066695430((?:/.+)+)(?=")""".toRegex(),
+					Regex.escapeReplacement(rootProject.toURI().toString().removeSuffix("/")) + "$1"
+				)
+	}
+
+	companion object {
+		private fun read(path: String): String =
+			fileFromResources(ViolationTestResources::class.java, path)
+	}
+}
+
+private fun fileFromResources(loader: Class<*>, path: String): String {
+	val container = "/${loader.`package`.name.replace(".", "/")}"
+	val fullPath = "${container}/${path}"
+	val resource = loader.getResource(fullPath)
+		?: throw IllegalArgumentException("Cannot find ${fullPath}, trying to load ${path} near ${loader}.")
+	return resource.readText()
+}
