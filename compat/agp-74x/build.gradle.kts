a19fcf4b30e0fd3a9fd3d9d2e1490a6c67ebc8fd
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-08-18 22:18:05 +0100

    Fix Java 11 issue

--- compat/agp-74x/build.gradle.kts
+++ compat/agp-74x/build.gradle.kts
@@ -11,3 +11,12 @@ dependencies {
 	implementation(gradleApiWithoutKotlin())
 	compileOnly(libs.android.gradle.v74x)
 }
+
+plugins.withId("java") {
+	// libs.android.gradle.v74x declares `org.gradle.jvm.version=11` as an attribute,
+	// which fails to resolve when using Java 8.
+	// Details: `gradlew :compat:agp-74x:dependencyInsight --dependency=com.android.tools.build:gradle`
+	val java = extensions.getByName<JavaPluginExtension>("java")
+	java.sourceCompatibility = JavaVersion.VERSION_11
+	java.targetCompatibility = JavaVersion.VERSION_11
+}
