76db3e6c5684f64bc353db35f042348370f9ea76
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-09-15 21:59:35 +0100

    Format

--- gradle/plugins/build.gradle.kts
+++ gradle/plugins/build.gradle.kts
@@ -10,7 +10,7 @@ gradlePlugin {
 			id = "net.twisterrob.gradle.plugins.settings"
 			implementationClass = "net.twisterrob.gradle.plugins.settings.SettingsPlugin"
 		}
-		
+
 		// Re-exposure of plugin from dependency. Gradle doesn't expose the plugin itself, even with api().
 		create("com.gradle.enterprise") {
 			id = "com.gradle.enterprise"
@@ -39,7 +39,7 @@ dependencies {
 	implementation(libs.kotlin.gradle)
 	implementation(libs.kotlin.dokka)
 	compileOnly(libs.nexus)
-	
+
 	// TODEL hack from https://github.com/gradle/gradle/issues/15383#issuecomment-779893192 (there are more parts to this)
 	compileOnly(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
 }
