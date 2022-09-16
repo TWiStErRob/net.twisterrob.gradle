7427877aec158384f8b41e2c7272a3193a85fc52
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-09-16 09:53:32 +0100

    Format project

--- gradle/plugins/src/main/kotlin/versionCatalogBuilder-load.kt
+++ gradle/plugins/src/main/kotlin/versionCatalogBuilder-load.kt
@@ -5,7 +5,7 @@ import java.io.File
 /**
  * Hack to be able to load multiple files into a [org.gradle.api.artifacts.VersionCatalog].
  * The built-in method is [VersionCatalogBuilder.from] which only allows one file.
- * 
+ *
  * This calls the internal function behind [VersionCatalogBuilder.from] skipping the checks.
  *
  * TODEL https://github.com/gradle/gradle/issues/20383#issuecomment-1236419331
