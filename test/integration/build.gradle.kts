e63460a73b325567adc08834af911de92ffcc21a
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-09-16 15:54:42 +0100

    Hide warning.

--- test/integration/build.gradle.kts
+++ test/integration/build.gradle.kts
@@ -11,3 +11,7 @@ dependencies {
 	testImplementation(projects.test.internal)
 	testImplementation(testFixtures(projects.plugin.base))
 }
+
+// Necessary to apply java-gradle-plugin so that the dependent plugins get exposed to tests correctly.
+// No own plugin is published from this project.
+disableGradlePluginValidation()
