3094802737baf688df49ec15988cdcf01551078a
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-09-17 19:04:49 +0100

    Only run test if passes

--- test/integration/build.gradle.kts
+++ test/integration/build.gradle.kts
@@ -15,3 +15,10 @@ dependencies {
 // Necessary to apply java-gradle-plugin so that the dependent plugins get exposed to tests correctly.
 // No own plugin is published from this project.
 disableGradlePluginValidation()
+
+tasks.withType<Test>().configureEach {
+	onlyIf {
+		it.project.property("net.twisterrob.test.android.pluginVersion").toString() == "7.3.0" &&
+		it.project.property("net.twisterrob.test.kotlin.pluginVersion").toString() == "1.6.21"
+	}
+}
