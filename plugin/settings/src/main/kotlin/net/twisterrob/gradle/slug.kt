c293d261b5a264e105a4e9b333ed986648aec44d
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-09-18 09:35:02 +0100

    Test new property: slug

--- plugin/settings/src/main/kotlin/net/twisterrob/gradle/slug.kt
+++ plugin/settings/src/main/kotlin/net/twisterrob/gradle/slug.kt
@@ -1,7 +1,15 @@
-package net.twisterrob.gradle.projects
+@file:JvmMultifileClass
+@file:JvmName("GradleUtils")
+
+package net.twisterrob.gradle
 
 import org.gradle.api.Project
 
+/**
+ * Generate a file-name-friendly identifier for a [Project].
+ * Better than [Project.getName], because that wouldn't be unique, but worse than [Project.getPath],
+ * because it could be ambiguous in some edge cases (:a:b and :a-b will have the same [slug]).
+ */
 val Project.slug: String
 	get() =
 		if (this == rootProject) {
