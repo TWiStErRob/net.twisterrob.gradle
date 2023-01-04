520cfe5c022ab61273fc0fde728275d5d8217de4
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-04 15:22:44 +0000

    Suppress warning for AGP 7.x vs Gradle 8.

--- docs/examples/release/build.gradle.kts
+++ docs/examples/release/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	id("com.android.application") version "7.4.0"
+	// TODEL Duplicate function hack from below, use plugin 0.15 variant.
 	id("net.twisterrob.quality") version "0.14.1"
 }
 
@@ -20,3 +21,36 @@ repositories {
 	google()
 	mavenCentral()
 }
+
+val agpVersion = com.android.Version.ANDROID_GRADLE_PLUGIN_VERSION
+if (!agpVersion.startsWith("7.")) {
+	error("AGP major version changed, review below hack.")
+}
+
+// Copy of plugin\settings\src\main\kotlin\net\twisterrob\gradle\doNotNagAbout.kt temporarily until 0.15 is released.
+fun doNotNagAbout(message: String) {
+	val logger: Any = org.gradle.internal.deprecation.DeprecationLogger::class.java
+		.getDeclaredField("DEPRECATED_FEATURE_HANDLER")
+		.apply { isAccessible = true }
+		.get(null)
+
+	@Suppress("UNCHECKED_CAST")
+	val messages: MutableSet<String> =
+		org.gradle.internal.featurelifecycle.LoggingDeprecatedFeatureHandler::class.java
+			.getDeclaredField("messages")
+			.apply { isAccessible = true }
+			.get(logger) as MutableSet<String>
+
+	messages.add(message)
+}
+
+val gradleVersion: String = GradleVersion.current().baseVersion.version
+
+// See https://issuetracker.google.com/issues/264177800
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"The Report.destination property has been deprecated. " +
+			"This is scheduled to be removed in Gradle 9.0. " +
+			"Please use the outputLocation property instead. " +
+			"See https://docs.gradle.org/${gradleVersion}/dsl/org.gradle.api.reporting.Report.html#org.gradle.api.reporting.Report:destination for more details."
+)
