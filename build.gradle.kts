88a2303e37d55e4394626258df762a0f0422e2d2
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-09 12:39:31 +0000

    Rerproducible builds (temporarily)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.kotlin.utils.keysToMap
-import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
 plugins {
@@ -160,7 +161,7 @@ subprojects {
 							"Implementation-Vendor" to project.group,
 							"Implementation-Title" to project.base.archivesName.get(),
 							"Implementation-Version" to project.version,
-							"Built-Date" to DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+							"Built-Date" to DateTimeFormatter.ISO_INSTANT.format(LocalDate.now().atStartOfDay().toInstant(ZoneOffset.UTC)), // STOPSHIP
 						)
 					)
 				}
