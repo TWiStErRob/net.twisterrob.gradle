f915e3d1e061ad5d3eb11e03b7203d6e23529157
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-20 13:19:06 +0000

    Gradle sync in IDEA 2022.3.1 uses deprecated AbstractArchiveTask.archivePath, suppress it. https://youtrack.jetbrains.com/issue/IDEA-306975

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import net.twisterrob.gradle.doNotNagAbout
 import org.jetbrains.kotlin.utils.keysToMap
 import java.time.Instant
 import java.time.format.DateTimeFormatter
@@ -300,3 +301,14 @@ nexusPublishing {
 		}
 	}
 }
+
+val gradleVersion: String = GradleVersion.current().version
+
+// TODEL Gradle sync in IDEA 2022.3.1: https://youtrack.jetbrains.com/issue/IDEA-306975
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"The AbstractArchiveTask.archivePath property has been deprecated. " +
+			"This is scheduled to be removed in Gradle 9.0. " +
+			"Please use the archiveFile property instead. " +
+			"See https://docs.gradle.org/${gradleVersion}/dsl/org.gradle.api.tasks.bundling.AbstractArchiveTask.html#org.gradle.api.tasks.bundling.AbstractArchiveTask:archivePath for more details."
+)
