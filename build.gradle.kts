e810f8c491984faeec7b83cc08a94da28cbdd4bc
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-20 13:05:02 +0000

    Remove redundant suppressions due to Gradle 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -111,7 +111,7 @@ subprojects {
 		properties.forEach { (name, value) -> value?.let { jvmArgs("-D${name}=${value}") } }
 	}
 
-	tasks.withType<@Suppress("UnstableApiUsage") ProcessResources>().configureEach {
+	tasks.withType<ProcessResources>().configureEach {
 		val propertyNamesToReplace = listOf(
 			"net.twisterrob.test.android.pluginVersion",
 			"net.twisterrob.test.kotlin.pluginVersion",
@@ -210,7 +210,6 @@ tasks.register("check") {
 project.tasks.register<TestReport>("testReport") {
 	group = LifecycleBasePlugin.VERIFICATION_GROUP
 	description = "Run and report on all tests in the project. Add `-x test` to just generate report."
-	@Suppress("UnstableApiUsage")
 	destinationDirectory.set(file("${buildDir}/reports/tests/all"))
 
 	val tests = subprojects
@@ -218,13 +217,11 @@ project.tasks.register<TestReport>("testReport") {
 		.onEach { it.ignoreFailures = true } // Let the tests finish, to get a final "all" report.
 	// Detach (.get()) the result directories,
 	// simply using reportOn(tests) or the binaryResultsDirectory providers, task dependencies would be created.
-	@Suppress("UnstableApiUsage")
 	testResults.from(tests.map { it.binaryResultsDirectory.get() })
 	// Force executing tests (if they're in the task graph), before reporting on them.
 	mustRunAfter(tests)
 
 	doLast {
-		@Suppress("UnstableApiUsage")
 		val reportFile = destinationDirectory.file("index.html").get().asFile
 		val failureRegex = Regex("""(?s).*<div class="infoBox" id="failures">\s*<div class="counter">(\d+)<\/div>.*""")
 		val failureMatch = failureRegex.matchEntire(reportFile.readText())
