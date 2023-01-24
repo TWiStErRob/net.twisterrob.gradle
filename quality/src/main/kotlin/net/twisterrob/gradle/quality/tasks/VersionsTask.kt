f8a07c79a5b09002ed2a51772260bda8c9190a62
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-24 17:19:10 +0000

    Update versions compatibility

--- quality/src/main/kotlin/net/twisterrob/gradle/quality/tasks/VersionsTask.kt
+++ quality/src/main/kotlin/net/twisterrob/gradle/quality/tasks/VersionsTask.kt
@@ -8,10 +8,7 @@ import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.UntrackedTask
 
 @UntrackedTask(because = "It is used to inspect Gradle state, output is console.")
-open class VersionsTask : DefaultTask() {
-// Note: Gradle convention is to make tasks abstract, but this task fails on Gradle 4.x with:
-// > Caused by: org.gradle.api.GradleException: Cannot create a proxy class for abstract class 'VersionsTask'.
-// without an actionable root cause.
+abstract class VersionsTask : DefaultTask() {
 
 	init {
 		outputs.upToDateWhen { false }
