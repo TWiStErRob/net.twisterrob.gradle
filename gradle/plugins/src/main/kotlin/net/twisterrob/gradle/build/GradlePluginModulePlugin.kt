f4985902561764ebd55d1f61cb82828afdfab147
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-27 10:03:11 +0000

    #360 Globalize Java hack

--- gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/GradlePluginModulePlugin.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/GradlePluginModulePlugin.kt
@@ -1,5 +1,6 @@
 package net.twisterrob.gradle.build;
 
+import net.twisterrob.gradle.build.compilation.JavaCompatibilityPlugin
 import net.twisterrob.gradle.build.detekt.DetektPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -10,6 +11,7 @@ class GradlePluginModulePlugin : Plugin<Project> {
 	override fun apply(project: Project) {
 		project.plugins.apply("org.gradle.java-gradle-plugin")
 		project.plugins.apply("org.jetbrains.kotlin.jvm")
+		project.plugins.apply(JavaCompatibilityPlugin::class)
 		//project.plugins.apply("org.gradle.java-test-fixtures")
 		project.plugins.apply(DetektPlugin::class)
 	}
