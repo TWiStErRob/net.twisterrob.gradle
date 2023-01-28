75080a502c5a0918944d30e8eceeab96f6983dc3
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-28 15:37:59 +0000

    Add docs

--- compat/gradle/src/main/kotlin/net/twisterrob/gradle/internal/deprecation/canNagUser.kt
+++ compat/gradle/src/main/kotlin/net/twisterrob/gradle/internal/deprecation/canNagUser.kt
@@ -6,6 +6,9 @@ package net.twisterrob.gradle.internal.deprecation
 import org.gradle.util.GradleVersion
 
 fun canNagUser(gradleVersion: GradleVersion): Boolean =
-	// STOPSHIP comments
+	// Gradle 6.3+ has a nagging mechanism,
+	// at that point there were some major changes to the internal DeprecationMessageBuilder.
+	// I think it's not worth going back more to support older versions with nagUserWith.
 	GradleVersion.version("6.3") <= gradleVersion.baseVersion
+			// Give it an upper limit too, as we don't know if it will keep working past Gradle 9.
 			&& gradleVersion.baseVersion <= GradleVersion.version("9.0")
