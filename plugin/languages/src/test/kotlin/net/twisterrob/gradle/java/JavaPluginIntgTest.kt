e43d61fdef75c9a9bb1b8049468e7831eeada55f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-29 14:38:13 +0000

    Increase Metaspace for 2 test classes.

--- plugin/languages/src/test/kotlin/net/twisterrob/gradle/java/JavaPluginIntgTest.kt
+++ plugin/languages/src/test/kotlin/net/twisterrob/gradle/java/JavaPluginIntgTest.kt
@@ -10,6 +10,7 @@ import net.twisterrob.gradle.test.assertNoOutputLine
 import net.twisterrob.gradle.test.assertNoSource
 import net.twisterrob.gradle.test.assertSuccess
 import org.intellij.lang.annotations.Language
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
@@ -22,6 +23,10 @@ import org.junit.jupiter.api.extension.ExtendWith
 class JavaPluginIntgTest : BaseAndroidIntgTest() {
 
 	override lateinit var gradle: GradleRunnerRule
+
+	@BeforeEach fun setMemory() {
+		gradle.file("org.gradle.jvmargs=-Xmx512M -XX:MaxMetaspaceSize=384M\n", "gradle.properties")
+	}
 
 	@Test fun `java plugin can be applied standalone`() {
 		@Language("gradle")
