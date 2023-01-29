425979edd7e4f7fc3a3b482377c80d6ae8a2004c
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-29 15:45:37 +0000

    Add stack overload for nagging.init.gradle

--- test/internal/runtime/src/main/kotlin/net/twisterrob/gradle/nagging/doNotNagAbout.kt
+++ test/internal/runtime/src/main/kotlin/net/twisterrob/gradle/nagging/doNotNagAbout.kt
@@ -15,11 +15,45 @@ import org.gradle.util.GradleVersion
  * @see net.twisterrob.gradle.nagging.doNotNagAboutPattern
  */
 fun doNotNagAbout(gradle: String, agpRegex: String, message: String) {
-	if (GradleVersion.current().baseVersion != GradleVersion.version(gradle)) return
-	if (!Regex(agpRegex).matches(agpVersion)) return
+	if (unsupported(gradle, agpRegex, "Ignoring deprecation: ${message}")) return
+	doNotNagAbout(message)
+}
+
+/**
+ * Overload for exact message matching with stack trace.
+ *
+ * @see doNotNagAbout for more details
+ * @see net.twisterrob.gradle.nagging.doNotNagAbout
+ */
+fun doNotNagAbout(gradle: String, agpRegex: String, message: String, stack: String) {
+	if (unsupported(gradle, agpRegex, "Ignoring deprecation: ${message} at ${stack}")) return
+	doNotNagAbout(message, stack)
+}
+
+/**
+ * Overload for more dynamic message matching.
+ *
+ * This method is named with a suffix of `Pattern` to make it easily available,
+ * because method references cannot pick up overloaded methods.
+ *
+ * @see doNotNagAbout for more details
+ * @see net.twisterrob.gradle.nagging.doNotNagAbout
+ */
+fun doNotNagAboutPattern(gradle: String, agpRegex: String, messageRegex: String) {
+	if (unsupported(gradle, agpRegex, "Ignoring deprecation regex: ${messageRegex}")) return
+	doNotNagAbout(Regex(messageRegex))
+}
+
+private fun unsupported(gradle: String, agpRegex: String, s: String): Boolean {
+	if (GradleVersion.current().baseVersion != GradleVersion.version(gradle)) {
+		return true
+	}
+
+	if (!Regex(agpRegex).matches(agpVersion)) {
+		return true
+	}
 
 	val logger = Logging.getLogger(Gradle::class.java)
-	logger.lifecycle("Ignoring deprecation: ${message}")
-
-	doNotNagAbout(message)
+	logger.lifecycle(s)
+	return false
 }
