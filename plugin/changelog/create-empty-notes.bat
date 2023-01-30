7b0fc92d484e23590d6122c61dfe326a614fbdde
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-30 14:11:53 +0000

    Split up changelog to files

---
+++ plugin/changelog/create-empty-notes.bat
@@ -0,0 +1,45 @@
+break > 0.6.1.md
+break > 0.7.1.md
+break > 0.8.0.md
+break > 0.8.1.md
+break > 0.8.2.md
+break > 0.8.3.md
+break > 0.8.4.md
+break > 0.8.4.1.md
+break > 0.8.4.2.md
+break > 0.8.5.md
+break > 0.9.0.md
+break > 0.9.1.md
+rem break > 2.3.3.10-19-26.1.md
+rem break > 3.0.1.10-19-26.0.md
+break > 3.0.1.10-19-26.2.md
+break > 3.0.1.10-19-26.3.md
+break > 3.0.1.10-19-26.4.md
+rem break > 3.1.3.14-27-27.0.md
+break > 3.1.3.14-27-27.1.md
+break > 3.1.3.14-27-27.2.md
+break > 3.1.4.14-27-27.0.md
+break > 3.1.4.14-27-27.1.md
+break > 3.1.4.14-27-27.2.md
+break > 3.1.4.14-27-27.3.md
+break > 3.1.4.14-28-28.0.md
+break > 3.1.4.14-28-28.1.md
+break > 3.1.4.14-28-28.2.md
+break > 3.2.1.14-28-28.0.md
+break > 3.2.1.14-28-28.1.md
+break > 3.2.1.14-28-28.2.md
+break > 3.2.1.14-28-28.3.md
+break > 3.3.2.14-28-28.0.md
+break > 3.3.2.14-28-28.1.md
+break > 3.3.2.14-28-28.2.md
+break > 3.4.2.14-28-28.0.md
+break > 3.4.2.14-29-29.0.md
+break > 3.4.2.14-29-29.1.md
+break > 3.5.4.14-29-29.0.md
+break > 3.6.4.14-29-29.0.md
+break > 4.0.2.14-29-29.0.md
+break > 4.0.2.14-29-29.1.md
+break > 4.0.2.14-29-29.2.md
+break > 4.1.0.14-29-29.0.md
+break > 4.1.1.14-29-29.0.md
+break > 4.1.1.14-29-29.1.md
