7fdbab5e5d83de34e280f3204f05aad807cf68b0
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-31 12:47:18 +0000

    Be more allowing for different IDEA versions (e.g. AS 2022.1.1 (EE) has closure4, IDEA 2022.3.2 has closure3.

--- build.gradle.kts
+++ build.gradle.kts
@@ -311,7 +311,7 @@ doNotNagAbout(
 			"This is scheduled to be removed in Gradle 9.0. " +
 			"Please use the archiveFile property instead. " +
 			"See https://docs.gradle.org/${gradleVersion}/dsl/org.gradle.api.tasks.bundling.AbstractArchiveTask.html#org.gradle.api.tasks.bundling.AbstractArchiveTask:archivePath for more details.",
-	"at org.jetbrains.plugins.gradle.tooling.builder.ExternalProjectBuilderImpl\$_getSourceSets_closure3"
+	"at org.jetbrains.plugins.gradle.tooling.builder.ExternalProjectBuilderImpl\$_getSourceSets_closure"
 )
 // TODEL Gradle sync in IDEA 2022.3.1: https://youtrack.jetbrains.com/issue/IDEA-306975
 @Suppress("MaxLineLength")
