751532947ea72c8e93802e33a7c8f134bd537fbf
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-02-01 22:33:01 +0000

    Need to stop editing outside of the IDE

--- common/src/test/kotlin/net/twisterrob/gradle/common/AGPVersionTest.kt
+++ common/src/test/kotlin/net/twisterrob/gradle/common/AGPVersionTest.kt
@@ -30,7 +30,7 @@ class AGPVersionTest {
 		@JvmStatic
 		fun mavenGoogle(): List<Arguments> =
 			listOf(
-				Arguments.of("7.4.1", AGPVersion(7, 4, Stable, 0)),
+				Arguments.of("7.4.1", AGPVersion(7, 4, Stable, 1)),
 				Arguments.of("7.4.0", AGPVersion(7, 4, Stable, 0)),
 				Arguments.of("7.4.0-rc02", AGPVersion(7, 4, Candidate, 2)),
 				Arguments.of("7.4.0-beta01", AGPVersion(7, 4, Beta, 1)),
