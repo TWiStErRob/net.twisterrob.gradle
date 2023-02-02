4978047d0a116f6ba99d07851004845179956aa8
Róbert Papp <papp.robert.s@gmail.com>
2023-02-02 17:07:32 +0000

    Verify Kotlin compatibility (#397)

--- gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/RootModulePlugin.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/RootModulePlugin.kt
@@ -4,10 +4,18 @@ import net.twisterrob.gradle.build.detekt.DetektRootPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
+import versionCatalogs
 
 class RootModulePlugin : Plugin<Project> {
 
 	override fun apply(project: Project) {
 		project.plugins.apply(DetektRootPlugin::class)
+
+		val libs = project.versionCatalogs.named("libs")
+		val target = libs.findVersion("kotlin-target").get().requiredVersion
+		val language = libs.findVersion("kotlin-language").get().requiredVersion
+		check(target.startsWith(language)) {
+			error("Kotlin target version ($target) must be compatible with language version ($language).")
+		}
 	}
 }
