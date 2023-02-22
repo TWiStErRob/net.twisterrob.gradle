c4f584bc13a67514f2f6e50967c6c6ce7c524475
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-02-22 10:03:20 +0000

    Code review

--- docs/examples/release/build.gradle.kts
+++ docs/examples/release/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
 	id("com.android.application") version "7.4.1"
-	// TODEL resolutionStrategy in settings.gradle.kts after changing to release 0.15
-	// TODO change plugin name after changing to release 0.15
-	// TODO enable doNotNagAbout below and make org.gradle.warning.mode=fail when updating to 0.15.
+	// TODEL resolutionStrategy in settings.gradle.kts after changing to release 0.15.1
+	// TODO change plugin name after changing to release 0.15.1
 	id("net.twisterrob.quality") version "0.15"
 }
 
