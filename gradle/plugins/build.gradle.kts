b1548fb8edf7c18b1bd06aadc036597e320e6fa4
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-01 21:52:16 +0000

    Make libs accessible everywhere correctly.

--- gradle/plugins/build.gradle.kts
+++ gradle/plugins/build.gradle.kts
@@ -55,8 +55,8 @@ dependencies {
 	implementation(libs.detekt)
 	compileOnly(libs.nexus)
 
-	// TODEL hack from https://github.com/gradle/gradle/issues/15383#issuecomment-779893192 (there are more parts to this)
-	compileOnly(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
+	// TODEL https://github.com/gradle/gradle/issues/15383
+	implementation(files(libs::class.java.superclass.protectionDomain.codeSource.location))
 }
 
 // Reusing code from :plugin:settings in the main project.
--- gradle/plugins/settings.gradle.kts
+++ gradle/plugins/settings.gradle.kts
@@ -1,7 +1,12 @@
+import org.gradle.api.internal.catalog.parser.TomlCatalogFileParser
+
 dependencyResolutionManagement {
 	versionCatalogs {
-		create("libs") {
-			from(files("../libs.versions.toml"))
+		create(defaultLibrariesExtensionName.get()) {
+			from(files("../../gradle/libs.versions.toml"))
+			// Note: it is necessary to load both libs and agp the same way as it's done
+			// in rootProject.settings.gradle.kts, because otherwise there are unresolved references.
+			TomlCatalogFileParser.parse(file("../../gradle/agp.versions.toml").inputStream(), this)
 		}
 	}
 }
