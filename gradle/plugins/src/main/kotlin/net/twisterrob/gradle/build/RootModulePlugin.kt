7d543505f3fbafd2c0f921196d0428d2da1abb66
Róbert Papp <papp.robert.s@gmail.com>
2023-03-01 22:52:38 +0000

    Clean up libs hack (#412)

--- gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/RootModulePlugin.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/RootModulePlugin.kt
@@ -1,19 +1,18 @@
 package net.twisterrob.gradle.build
 
+import libs
 import net.twisterrob.gradle.build.detekt.DetektRootPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
-import versionCatalogs
 
 class RootModulePlugin : Plugin<Project> {
 
 	override fun apply(project: Project) {
 		project.plugins.apply(DetektRootPlugin::class)
 
-		val libs = project.versionCatalogs.named("libs")
-		val target = libs.findVersion("kotlin-target").get().requiredVersion
-		val language = libs.findVersion("kotlin-language").get().requiredVersion
+		val target = project.libs.versions.kotlin.target.get()
+		val language = project.libs.versions.kotlin.language.get()
 		check(target.startsWith(language)) {
 			error("Kotlin target version ($target) must be compatible with language version ($language).")
 		}
