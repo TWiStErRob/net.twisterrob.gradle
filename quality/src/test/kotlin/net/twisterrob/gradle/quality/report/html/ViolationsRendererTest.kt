dad8b834c03855d7633ab6bf59d144b4e31b7b4d
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-11 13:54:28 +0000

    Reproduce #423 in test

--- quality/src/test/kotlin/net/twisterrob/gradle/quality/report/html/ViolationsRendererTest.kt
+++ quality/src/test/kotlin/net/twisterrob/gradle/quality/report/html/ViolationsRendererTest.kt
@@ -13,6 +13,8 @@ import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.EnabledOnOs
+import org.junit.jupiter.api.condition.OS
 import org.junit.jupiter.api.io.TempDir
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
@@ -150,6 +152,86 @@ class ViolationsRendererTest {
 			location.setField("startLine", 3)
 			location.setField("endLine", 5)
 			location.generateTestContent()
+		}
+		val fixtCategory: Category = fixture.build()
+		val fixtReporter: Reporter = fixture.build()
+		val violations: Map<Category, Map<Reporter, List<Violation>>> =
+			mapOf(fixtCategory to mapOf(fixtReporter to listOf(fixtViolation)))
+
+		@Language("XML")
+		val expected = """<?xml version="1.0" encoding="utf-8"?>
+			<violations project="test project">
+			<category name="${fixtCategory}">
+			<reporter name="${fixtReporter}">
+			<violation>
+				<location
+					 module="${fixtViolation.location.module.path}"
+					 modulePrefix=""
+					 moduleName=""
+					 variant="${fixtViolation.location.variant}"
+					 file="${fixtViolation.location.file.absolutePath}"
+					 fileName="${fixtViolation.location.file.name}"
+					 fileAbsoluteAsUrl="${fixtViolation.location.file.toURI()}"
+					 pathRelativeToProject=".${File.separator}"
+					 pathRelativeToModule=".${File.separator}"
+					 fileIsExternal="true"
+					 startLine="${fixtViolation.location.startLine}"
+					 endLine="${fixtViolation.location.endLine}"
+					 column="${fixtViolation.location.column}">
+				</location>
+				<source
+					 parser="${fixtViolation.source.parser}"
+					 source="${fixtViolation.source.source}"
+					 reporter="${fixtViolation.source.reporter}">
+				</source>
+				<details
+					 rule="${fixtViolation.rule}"
+					 category="${fixtViolation.category}"
+					 severity="${fixtViolation.severity}">
+					<message><![CDATA[${fixtViolation.message}]]></message>
+					<context type="code" language="binary"
+						 startLine="${fixtViolation.location.startLine - 2}"
+						 endLine="${fixtViolation.location.endLine + 2}">
+						<![CDATA[
+							Line 1\n
+							Line 2\n
+							Line 3\n
+							Line 4\n
+							Line 5\n
+							Line 6\n
+							Line 7
+						]]>
+					</context>
+				</details>
+				<specific
+					 key="${fixtViolation.specifics.asSequence().elementAt(0).key}"
+					 value="${fixtViolation.specifics.asSequence().elementAt(0).value}">
+				</specific>
+				<specific
+					 key="${fixtViolation.specifics.asSequence().elementAt(1).key}"
+					 value="${fixtViolation.specifics.asSequence().elementAt(1).value}">
+				</specific>
+				<specific
+					 key="${fixtViolation.specifics.asSequence().elementAt(2).key}"
+					 value="${fixtViolation.specifics.asSequence().elementAt(2).value}">
+				</specific>
+			</violation>
+			</reporter>
+			</category>
+			</violations>
+		""".trimIndent()
+
+		val rendered = render(violations)
+
+		assertEquals(expected.unformat(), rendered)
+	}
+
+	@EnabledOnOs(OS.WINDOWS)
+	@Suppress("LongMethod")
+	@Test fun `renderXml renders a violation in a real project with external file`(@TempDir temp: File) {
+		val fixtViolation: Violation = fixture.build {
+			location.setField("module", ProjectBuilder().withProjectDir(temp).build())
+			location.setField("file", File("A:\\${fixture.build<String>()}"))
 		}
 		val fixtCategory: Category = fixture.build()
 		val fixtReporter: Reporter = fixture.build()
