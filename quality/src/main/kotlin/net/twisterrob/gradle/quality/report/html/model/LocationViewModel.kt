338521c9d944c2eb6c1d5d666f36d491a777eb10
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-11 16:09:53 +0000

    Format and document LocationViewModel

--- quality/src/main/kotlin/net/twisterrob/gradle/quality/report/html/model/LocationViewModel.kt
+++ quality/src/main/kotlin/net/twisterrob/gradle/quality/report/html/model/LocationViewModel.kt
@@ -18,7 +18,8 @@ class LocationViewModel(violation: Violation) {
 	 *
 	 * Since [modulePrefix] and [moduleName] can be empty, this will always give the right result.
 	 */
-	val modulePath: String get() = module.path
+	val modulePath: String
+		get() = module.path
 
 	/**
 	 * The prefix leading up to the name of the module, but not including the separator.
@@ -48,26 +49,51 @@ class LocationViewModel(violation: Violation) {
 				module.name
 			}
 
-	val variant: String get() = loc.variant
-	val file: String get() = loc.file.absolutePath
-	val fileName: String get() = loc.file.name
-	val fileAbsoluteAsUrl: URI get() = loc.file.absoluteFile.toURI()
+	val variant: String
+		get() = loc.variant
 
+	val file: String
+		get() = loc.file.absolutePath
+
+	val fileName: String
+		get() = loc.file.name
+
+	val fileAbsoluteAsUrl: URI
+		get() = loc.file.absoluteFile.toURI()
+
+	/**
+	 * The directory of the file, relative to the project root, so the absolute path is:
+	 * `rootProject.projectDir` + [locationRelativeToProject] + [fileName]
+	 */
 	val locationRelativeToProject: String
 		get() = module.rootProject.relativePath(loc.file.parentFile) + File.separator
 
+	/**
+	 * The directory of the file, relative to the module root, so the absolute path is:
+	 * `module.projectDir` + [locationRelativeToModule] + [fileName]
+	 */
 	val locationRelativeToModule: String
 		get() = module.relativePath(loc.file.parentFile) + File.separator
 
 	/**
+	 * Whether this location is outside the project root.
 	 * Files are internal if they're located within the project / root module.
-	 * 
+	 * This could be just one level up, or even a different drive on Windows.
+	 * This would imply that it's better to use absolute paths in some places
+	 * rather than [locationRelativeToProject] or [locationRelativeToModule].
+	 * [locationRelativeToProject] or [locationRelativeToModule] will be absolute when this is true.
+	 *
 	 * Assumption: the root project physically contains all the other modules.
 	 */
 	val isLocationExternal: Boolean
 		get() = (module.rootProject.relativePath(loc.file.parentFile) + File.separator).startsWith("")
 
-	val startLine: Int get() = loc.startLine
-	val endLine: Int get() = loc.endLine
-	val column: Int get() = loc.column
+	val startLine: Int
+		get() = loc.startLine
+
+	val endLine: Int
+		get() = loc.endLine
+
+	val column: Int
+		get() = loc.column
 }
