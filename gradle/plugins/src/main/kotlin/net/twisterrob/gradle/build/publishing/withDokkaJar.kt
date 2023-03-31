1da10cbec133136b648ea334b7cb83672e1ee078
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-31 13:56:06 +0100

    Fix internal API usages

--- gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/publishing/withDokkaJar.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/publishing/withDokkaJar.kt
@@ -3,14 +3,12 @@ package net.twisterrob.gradle.build.publishing
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.attributes.DocsType
-import org.gradle.api.component.AdhocComponentWithVariants
+import org.gradle.api.internal.project.ProjectInternal
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.plugins.internal.JvmPluginsHelper
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.TaskProvider
-import org.gradle.kotlin.dsl.get
-import org.gradle.kotlin.dsl.support.serviceOf
 
 /**
  * @param project [Project] this extension belongs to
@@ -18,18 +16,13 @@ import org.gradle.kotlin.dsl.support.serviceOf
  * @see org.gradle.api.plugins.internal.DefaultJavaPluginExtension.withJavadocJar
  */
 fun JavaPluginExtension.withDokkaJar(project: Project, artifactTask: TaskProvider<out Task>) {
-	JvmPluginsHelper.configureDocumentationVariantWithArtifact(
+	JvmPluginsHelper.createDocumentationVariantWithArtifact(
 		JavaPlugin.JAVADOC_ELEMENTS_CONFIGURATION_NAME,
 		null,
 		DocsType.JAVADOC,
 		emptyList(),
 		sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME).javadocJarTaskName,
 		artifactTask,
-		project.components["java"] as AdhocComponentWithVariants,
-		project.configurations,
-		project.tasks,
-		project.objects,
-		project.serviceOf(),
-		project.serviceOf(),
+		project as ProjectInternal,
 	)
 }
