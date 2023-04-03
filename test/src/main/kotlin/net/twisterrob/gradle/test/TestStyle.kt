5e2b239dac60561320274cc81dcc84f91c9d7445
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-04-03 16:58:15 +0100

    Fail if there are namespace warnings remaining

--- test/src/main/kotlin/net/twisterrob/gradle/test/TestStyle.kt
+++ test/src/main/kotlin/net/twisterrob/gradle/test/TestStyle.kt
@@ -4,7 +4,10 @@ import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 
 inline fun GradleRunnerRule.runBuild(block: GradleRunnerRule.() -> GradleRunner): BuildResult =
-	this.block().build()
+	this.block().build().also {
+		// STOPSHIP
+		it.assertNoOutputLine("Setting the namespace via a source AndroidManifest.xml's package attribute is deprecated.")
+	}
 
 inline fun GradleRunnerRule.runFailingBuild(block: GradleRunnerRule.() -> GradleRunner): BuildResult =
 	this.block().buildAndFail()
