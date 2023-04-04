f6a66888dacbfc345c16b69d4643bdc0befe3ffa
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-04-04 13:26:09 +0100

    Remove check

--- test/src/main/kotlin/net/twisterrob/gradle/test/TestStyle.kt
+++ test/src/main/kotlin/net/twisterrob/gradle/test/TestStyle.kt
@@ -4,10 +4,7 @@ import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 
 inline fun GradleRunnerRule.runBuild(block: GradleRunnerRule.() -> GradleRunner): BuildResult =
-	this.block().build().apply {
-		// STOPSHIP
-		assertNoOutputLine("Setting the namespace via a source AndroidManifest.xml's package attribute is deprecated.")
-	}
+	this.block().build()
 
 inline fun GradleRunnerRule.runFailingBuild(block: GradleRunnerRule.() -> GradleRunner): BuildResult =
 	this.block().buildAndFail()
