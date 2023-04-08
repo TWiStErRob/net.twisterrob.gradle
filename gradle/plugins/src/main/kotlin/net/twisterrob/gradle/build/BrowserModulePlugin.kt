06fca4c02ddfea745d38ed1d4b197fd0fd0d92ca
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-04-08 22:37:31 +0100

    Typo

--- gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/BrowserModulePlugin.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/BrowserModulePlugin.kt
@@ -13,7 +13,7 @@ class BrowserModulePlugin : Plugin<Project> {
 			this.isCanBeResolved = true
 			this.isCanBeConsumed = false
 		}
-		// TODO reduce this to just the bare minimum required by IDEA which is unknown at the momemnt.
+		// TODO reduce this to just the bare minimum required by IDEA which is unknown at the moment.
 		// It might be as simple as creating a configuration with the right name, or right attributes.
 		project.plugins.apply("org.gradle.java")
 		project.configurations.named(JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME) {
