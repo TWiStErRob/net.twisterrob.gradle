93b4e03adb7c986b6c426bf81ab5804621475f2b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-04-10 19:35:58 +0100

    Expose nagging utilities to settings.gradle too.

--- test/internal/runtime/src/main/kotlin/net/twisterrob/gradle/nagging/NaggingPlugin.kt
+++ test/internal/runtime/src/main/kotlin/net/twisterrob/gradle/nagging/NaggingPlugin.kt
@@ -2,31 +2,35 @@ package net.twisterrob.gradle.nagging
 
 import org.gradle.api.Plugin
 import org.gradle.api.invocation.Gradle
+import org.gradle.api.plugins.ExtensionAware
 
 /**
  * Used from `init.gradle.kts` in test resources.
  */
 class NaggingPlugin : Plugin<Gradle> {
 	override fun apply(gradle: Gradle) {
-		gradle.rootProject { rootProject ->
-			// Groovy .ext === Kotlin .extensions.extraProperties === Kotlin DSL .extra
-			// Based on https://stackoverflow.com/a/19269037/253468
-			// Based on https://discuss.gradle.org/t/how-to-access-a-function-defined-in-init-gradle-in-build-script/6200/2
+		gradle.rootProject(::exposeDoNotNagAboutExtras)
+		gradle.beforeSettings(::exposeDoNotNagAboutExtras)
+	}
 
-			// Access from build.gradle:
-			// def doNotNagAbout = rootProject.ext["doNotNagAbout"]
-			// doNotNagAbout("7.4.2", "^7\\.2\\.\\d+\$", "message")
+	private fun exposeDoNotNagAboutExtras(extensionAware: ExtensionAware) {
+		// Groovy .ext === Kotlin .extensions.extraProperties === Kotlin DSL .extra
+		// Based on https://stackoverflow.com/a/19269037/253468
+		// Based on https://discuss.gradle.org/t/how-to-access-a-function-defined-in-init-gradle-in-build-script/6200/2
 
-			// Access from build.gradle.kts:
-			// val doNotNagAbout = project.rootProject.extra["doNotNagAbout"] as (String, String, String) -> Unit
-			// val doNotNagAbout = project.rootProject.extensions.extraProperties["doNotNagAbout"] as (String, String, String) -> Unit
-			// doNotNagAbout("7.4.2", """^7\.2\.\d$""", "message")
+		// Access from build.gradle:
+		// def doNotNagAbout = rootProject.ext["doNotNagAbout"]
+		// doNotNagAbout("7.4.2", "^7\\.2\\.\\d+\$", "message")
 
-			val doNotNagAboutRef: (String, String, String) -> Unit = ::doNotNagAbout
-			rootProject.extensions.extraProperties.set("doNotNagAbout", doNotNagAboutRef)
-			val doNotNagAboutStackRef: (String, String, String, String) -> Unit = ::doNotNagAbout
-			rootProject.extensions.extraProperties.set("doNotNagAboutStack", doNotNagAboutStackRef)
-			rootProject.extensions.extraProperties.set("doNotNagAboutPattern", ::doNotNagAboutPattern)
-		}
+		// Access from build.gradle.kts:
+		// val doNotNagAbout = project.rootProject.extra["doNotNagAbout"] as (String, String, String) -> Unit
+		// val doNotNagAbout = project.rootProject.extensions.extraProperties["doNotNagAbout"] as (String, String, String) -> Unit
+		// doNotNagAbout("7.4.2", """^7\.2\.\d$""", "message")
+
+		val doNotNagAboutRef: (String, String, String) -> Unit = ::doNotNagAbout
+		extensionAware.extensions.extraProperties.set("doNotNagAbout", doNotNagAboutRef)
+		val doNotNagAboutStackRef: (String, String, String, String) -> Unit = ::doNotNagAbout
+		extensionAware.extensions.extraProperties.set("doNotNagAboutStack", doNotNagAboutStackRef)
+		extensionAware.extensions.extraProperties.set("doNotNagAboutPattern", ::doNotNagAboutPattern)
 	}
 }
