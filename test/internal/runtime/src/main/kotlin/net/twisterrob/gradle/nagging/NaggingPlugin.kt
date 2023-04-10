b4eb0cbeb92d8236f73e9190c1adf9f382dea8f4
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-04-10 21:52:52 +0100

    Improve docs

--- test/internal/runtime/src/main/kotlin/net/twisterrob/gradle/nagging/NaggingPlugin.kt
+++ test/internal/runtime/src/main/kotlin/net/twisterrob/gradle/nagging/NaggingPlugin.kt
@@ -13,20 +13,31 @@ class NaggingPlugin : Plugin<Gradle> {
 		gradle.beforeSettings(::exposeDoNotNagAboutExtras)
 	}
 
+	/**
+	 * Expose [doNotNagAbout] to build scripts.
+	 *
+	 * Groovy `.ext` === Kotlin `.extensions.extraProperties` === Kotlin DSL `.extra`.
+	 * Based on [Function from init.gradle in build script](https://stackoverflow.com/a/19269037/253468).
+	 * Based on [How to access a function defined in init.gradle in build script](https://discuss.gradle.org/t/6200/2)
+	 *
+	 * Examples are for `rootProject`, replace `rootProject` with `settings` for [org.gradle.api.initialization.Settings].
+	 *
+	 * Access from `build.gradle`:
+	 * ```groovy
+	 * def doNotNagAbout = rootProject.ext["doNotNagAbout"]
+	 * doNotNagAbout("7.4.2", "^7\\.2\\.\\d+\$", "message")
+	 * ```
+	 *
+	 * Access from `build.gradle.kts`:
+	 * ```kotlin
+	 * val doNotNagAbout = rootProject.extra["doNotNagAbout"] as (String, String, String) -> Unit
+	 * val doNotNagAbout = rootProject.extensions.extraProperties["doNotNagAbout"] as (String, String, String) -> Unit
+	 * doNotNagAbout("7.4.2", """^7\.2\.\d$""", "message")
+	 * ```
+	 *
+	 * Similar functions need different signatures!
+	 */
 	private fun exposeDoNotNagAboutExtras(extensionAware: ExtensionAware) {
-		// Groovy .ext === Kotlin .extensions.extraProperties === Kotlin DSL .extra
-		// Based on https://stackoverflow.com/a/19269037/253468
-		// Based on https://discuss.gradle.org/t/how-to-access-a-function-defined-in-init-gradle-in-build-script/6200/2
-
-		// Access from build.gradle:
-		// def doNotNagAbout = rootProject.ext["doNotNagAbout"]
-		// doNotNagAbout("7.4.2", "^7\\.2\\.\\d+\$", "message")
-
-		// Access from build.gradle.kts:
-		// val doNotNagAbout = project.rootProject.extra["doNotNagAbout"] as (String, String, String) -> Unit
-		// val doNotNagAbout = project.rootProject.extensions.extraProperties["doNotNagAbout"] as (String, String, String) -> Unit
-		// doNotNagAbout("7.4.2", """^7\.2\.\d$""", "message")
-
 		val doNotNagAboutRef: (String, String, String) -> Unit = ::doNotNagAbout
 		extensionAware.extensions.extraProperties.set("doNotNagAbout", doNotNagAboutRef)
 		val doNotNagAboutStackRef: (String, String, String, String) -> Unit = ::doNotNagAbout
