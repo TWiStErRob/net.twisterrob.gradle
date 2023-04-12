c37c20bae5e7fb261c3ca25f1811dffeef385068
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-04-12 11:50:29 +0100

    Fix dokka publishing

--- gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/publishing/withDokkaJar.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/publishing/withDokkaJar.kt
@@ -3,26 +3,33 @@ package net.twisterrob.gradle.build.publishing
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.attributes.DocsType
+import org.gradle.api.component.AdhocComponentWithVariants
 import org.gradle.api.internal.project.ProjectInternal
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.plugins.internal.JavaConfigurationVariantMapping
 import org.gradle.api.plugins.internal.JvmPluginsHelper
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.TaskProvider
+import org.gradle.kotlin.dsl.getByName
 
 /**
  * @param project [Project] this extension belongs to
  * @param artifactTask that produces the Documentation output files
+ *
  * @see org.gradle.api.plugins.internal.DefaultJavaPluginExtension.withJavadocJar
+ * @see org.gradle.jvm.component.internal.DefaultJvmSoftwareComponent.enableJavadocJarVariant
  */
 fun JavaPluginExtension.withDokkaJar(project: Project, artifactTask: TaskProvider<out Task>) {
-	JvmPluginsHelper.createDocumentationVariantWithArtifact(
+	val kdocVariant = JvmPluginsHelper.createDocumentationVariantWithArtifact(
 		JavaPlugin.JAVADOC_ELEMENTS_CONFIGURATION_NAME,
 		null,
 		DocsType.JAVADOC,
 		emptyList(),
-		sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME).javadocJarTaskName,
+		this.sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME).javadocJarTaskName,
 		artifactTask,
 		project as ProjectInternal,
 	)
+	val java = project.components.getByName<AdhocComponentWithVariants>("java")
+	java.addVariantsFromConfiguration(kdocVariant, JavaConfigurationVariantMapping("runtime", true))
 }
