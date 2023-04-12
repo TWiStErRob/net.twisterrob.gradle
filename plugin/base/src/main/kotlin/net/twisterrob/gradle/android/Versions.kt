1d2c95642f9e28bb1456ac4fdc4f206f6edd7e52
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-04-12 22:37:49 +0100

    Bump all Android versions

--- plugin/base/src/main/kotlin/net/twisterrob/gradle/android/Versions.kt
+++ plugin/base/src/main/kotlin/net/twisterrob/gradle/android/Versions.kt
@@ -3,30 +3,32 @@
 package net.twisterrob.gradle.android
 
 /**
- * Keep it in sync with AppCompat's minimum.
+ * Keep it in sync with AndroidX / JetPack's minimum.
  */
 const val VERSION_SDK_MINIMUM: Int = 14
 
 /**
  * Latest SDK version available, Google Play Store has stringent rules, so keep up to date.
  */
-const val VERSION_SDK_TARGET: Int = 30
+const val VERSION_SDK_TARGET: Int = 33
 
 /**
  * Latest SDK version available, useful for discovering deprecated methods and getting new features like `.findViewById<T>()`.
  *
  * @see com.android.sdklib.SdkVersionInfo.HIGHEST_SUPPORTED_API
  */
-const val VERSION_SDK_COMPILE: Int = 30
+const val VERSION_SDK_COMPILE: Int = 33
 
 /**
  * Note: format changed at 9 Pie, was 8.1.0 Oreo.
+ *
+ * @see https://apilevels.com/
  */
-const val VERSION_SDK_COMPILE_NAME: String = "11" // Android 11 (R)
+const val VERSION_SDK_COMPILE_NAME: String = "13" // Android 13 (T)
 
 /**
  * Latest build tools version available, there's no reason to hold back.
  *
  * @see com.android.SdkConstants.CURRENT_BUILD_TOOLS_VERSION
  */
-const val VERSION_BUILD_TOOLS: String = "32.0.0"
+const val VERSION_BUILD_TOOLS: String = "33.0.2"
