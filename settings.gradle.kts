5600c3ad85c97e60ee9a25fa204603019f6009f8
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-05-27 17:13:34 +0100

    Suppress IDEA Sync vs Gradle 8.2 warnings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -116,3 +116,56 @@ if (gradleVersion == "8.2-milestone-1") {
 	val error: (String) -> Unit = if (isCI) ::error else logger::warn
 	error("Gradle version changed, please remove hack.")
 }
+
+// TODEL Gradle 8.2 milestone 1 sync in IDEA 2023.1 https://youtrack.jetbrains.com/issue/IDEA-320266.
+@Suppress("MaxLineLength", "StringLiteralDuplication")
+if ((System.getProperty("idea.version") ?: "") < "2023.2") {
+	// There are ton of warnings, ignoring them all by their class names in one suppression.
+	doNotNagAbout(
+		Regex(
+			"^" +
+					"(" +
+					Regex.escape("The Project.getConvention() method has been deprecated. ") +
+					"|" +
+					Regex.escape("The org.gradle.api.plugins.Convention type has been deprecated. ") +
+					"|" +
+					Regex.escape("The org.gradle.api.plugins.JavaPluginConvention type has been deprecated. ") +
+					")" +
+					Regex.escape("This is scheduled to be removed in Gradle 9.0. ") +
+					Regex.escape("Consult the upgrading guide for further information: ") +
+					Regex.escape("https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#") +
+					".*" +
+					"(" +
+					Regex.escape("at org.jetbrains.kotlin.idea.gradleTooling.KotlinTasksPropertyUtilsKt.") +
+					"|" +
+					Regex.escape("at org.jetbrains.plugins.gradle.tooling.util.JavaPluginUtil.") +
+					"|" +
+					Regex.escape("at org.jetbrains.plugins.gradle.tooling.builder.ExternalProjectBuilderImpl.") +
+					"|" +
+					Regex.escape("at org.jetbrains.plugins.gradle.tooling.builder.ProjectExtensionsDataBuilderImpl.") +
+					")" +
+					".*$"
+		)
+	)
+} else {
+	val error: (String) -> Unit = if (isCI) ::error else logger::warn
+	error("IDEA version changed, please review hack.")
+}
+
+// TODEL Gradle 8.2 milestone 1 sync in IDEA 2023.1 https://youtrack.jetbrains.com/issue/IDEA-320307.
+@Suppress("MaxLineLength", "StringLiteralDuplication")
+if ((System.getProperty("idea.version") ?: "") < "2023.2") {
+	@Suppress("MaxLineLength", "StringLiteralDuplication")
+	doNotNagAbout(
+		"The BuildIdentifier.getName() method has been deprecated. " +
+				"This is scheduled to be removed in Gradle 9.0. " +
+				"Use getBuildPath() to get a unique identifier for the build. " +
+				"Consult the upgrading guide for further information: " +
+				"https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#build_identifier_name_and_current_deprecation",
+		// There are 4 stack traces coming to this line, ignore them all at once.
+		"at org.jetbrains.plugins.gradle.tooling.util.resolve.DependencyResolverImpl.resolveDependencies(DependencyResolverImpl.java:266)"
+	)
+} else {
+	val error: (String) -> Unit = if (isCI) ::error else logger::warn
+	error("IDEA version changed, please review hack.")
+}
