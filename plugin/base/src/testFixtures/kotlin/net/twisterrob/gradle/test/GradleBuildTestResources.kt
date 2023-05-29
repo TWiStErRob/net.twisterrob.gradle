9f5a87cc7f03b734e10de58e1964b95d69550db4
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-05-29 22:30:55 +0100

    Keep and expand Kotlin base files for easy prototyping.

--- plugin/base/src/testFixtures/kotlin/net/twisterrob/gradle/test/GradleBuildTestResources.kt
+++ plugin/base/src/testFixtures/kotlin/net/twisterrob/gradle/test/GradleBuildTestResources.kt
@@ -8,16 +8,20 @@ object GradleBuildTestResources {
 	val android: AndroidProject = object : AndroidProject {}
 
 	fun GradleRunnerRule.basedOn(project: KotlinProject) {
+		file(project.build, ContentMergeMode.MERGE_GRADLE, "build.gradle")
 		file(project.settings, ContentMergeMode.MERGE_GRADLE, "settings.gradle.kts")
 	}
 
 	fun GradleRunnerRule.basedOn(project: AndroidProject) {
 		file(project.build, ContentMergeMode.MERGE_GRADLE, "build.gradle")
-		file(project.settings, "settings.gradle.kts")
+		file(project.settings, ContentMergeMode.MERGE_GRADLE, "settings.gradle.kts")
 		file(project.manifest, "src", "main", "AndroidManifest.xml")
 	}
 
 	interface KotlinProject {
+		val build: String
+			get() = read("kotlin-plugin_app/build.gradle")
+
 		val settings: String
 			get() = read("kotlin-plugin_app/settings.gradle.kts")
 	}
