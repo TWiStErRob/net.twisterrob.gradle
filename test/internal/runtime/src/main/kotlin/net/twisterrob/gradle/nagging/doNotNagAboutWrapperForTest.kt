518c9b95d17e07b0c11f588dffe4ecc36a38ef07
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-05-30 14:08:22 +0100

    Ignore detekt warning

--- test/internal/runtime/src/main/kotlin/net/twisterrob/gradle/nagging/doNotNagAboutWrapperForTest.kt
+++ test/internal/runtime/src/main/kotlin/net/twisterrob/gradle/nagging/doNotNagAboutWrapperForTest.kt
@@ -48,6 +48,7 @@ fun doNotNagAboutPatternForTest(gradle: Pair<String, String>, agp: Pair<String, 
 	doNotNagAbout(Regex(messageRegex))
 }
 
+@Suppress("NamedArguments") // Variable names are clear enough.
 private fun unsupported(gradle: Pair<String, String>, agp: Pair<String, String>, s: String): Boolean {
 	val logger = Logging.getLogger(Gradle::class.java)
 
