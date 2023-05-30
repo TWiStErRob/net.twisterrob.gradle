0ed2b97feba0f1c9cdfc9e317939b7e555f123c8
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-05-30 15:11:07 +0100

    Build with latest alpha

--- gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/compilation/JavaCompatibilityPlugin.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/compilation/JavaCompatibilityPlugin.kt
@@ -1,50 +1,77 @@
 package net.twisterrob.gradle.build.compilation
 
+import net.twisterrob.gradle.build.dsl.libs
+import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.CacheableRule
+import org.gradle.api.artifacts.ComponentMetadataContext
+import org.gradle.api.artifacts.ComponentMetadataRule
+import org.gradle.api.attributes.java.TargetJvmVersion
+import org.gradle.kotlin.dsl.withModule
+import javax.inject.Inject
 
 class JavaCompatibilityPlugin : Plugin<Project> {
 	override fun apply(target: Project) {
 		target.dependencies.components {
 			// AGP 8.0.0-alpha10 requires Java 17 (https://issuetracker.google.com/issues/241546506)
 			// But Google didn't change the Java compiler and class file format to be Java 17 yet.
-			// Even AGP 8.1.0-alpha11 is published with Java 11 bytecode
+			// Even AGP 8.1.0-beta03 is published with Java 11 bytecode
 			// according to its gradle-module-metadata and class header.
-
-			// =================================================================================================
-			// The original hack to downgrade Java 11 to Java 8 is left here, because expect it'll be back soon.
-			// =================================================================================================
+			// AGP 8.2.0-alpha03 onwards is published with Java 17 bytecode according to its metadata.
 
 			// ```
-			// Execution failed for task ':compat:agp-74x:compileKotlin'.
-			// > Could not resolve all files for configuration ':compat:agp-74x:compileClasspath'.
-			// > Could not resolve com.android.tools.build:gradle:7.4.0-alpha09.
-			// Required by: project :compat:agp-74x
-			// > No matching variant of com.android.tools.build:gradle:7.4.0-alpha09 was found.
-			// The consumer was configured to find an API of a library compatible with Java 8,
-			// preferably in the form of class files, preferably optimized for standard JVMs,
+			// Execution failed for task ':common:compileKotlin'.
+			// > Could not resolve all files for configuration ':common:compileClasspath'.
+			// > Could not resolve com.android.tools.build:gradle:8.2.0-alpha05.
+			// Required by: project :common
+			// > No matching variant of com.android.tools.build:gradle:8.2.0-alpha05 was found.
+			// The consumer was configured to find a library for use during compile-time, compatible with Java 11,
+			// preferably not packaged as a jar, preferably optimized for standard JVMs,
 			// and its dependencies declared externally,
 			// as well as attribute 'org.jetbrains.kotlin.platform.type' with value 'jvm' but:
-			// - Variant 'apiElements' capability com.android.tools.build:gradle:7.4.0-alpha09 declares ...:
-			// - Incompatible because this component declares a component compatible with Java 11
-			// and the consumer needed a component compatible with Java 8
+			// - Variant 'apiElements' capability com.android.tools.build:gradle:8.2.0-alpha05 declares ...:
+			// - Incompatible because this component declares a component, compatible with Java 17
+			// and the consumer needed a component, compatible with Java 11
 			// ```
-			// This project still uses Java 8, so let's rewrite the metadata,
-			// so that the produced jars can still be used with Java 8.
+			// This project still uses Java 11 for compatibility with AGP 7.x.
+			// Let's rewrite the metadata of AGP 8.2, so that the produced jars can still be used with Java 11.
 			// https://docs.gradle.org/current/userguide/component_metadata_rules.html
 
-			//val javaVersion = target.libs.versions.java.get()
-			//	.let { JavaVersion.toVersion(it).majorVersion.toInt() }
+			val javaVersion = target.libs.versions.java.get()
+				.let { JavaVersion.toVersion(it).majorVersion.toInt() }
 
-			//withModule<TargetJvmVersionRule>("com.android.tools.build:gradle") { params(javaVersion) }
-			//withModule<TargetJvmVersionRule>("com.android.tools.build:gradle-api") { params(javaVersion) }
-			//withModule<TargetJvmVersionRule>("com.android.tools.build:gradle-settings-api") { params(javaVersion) }
-			//withModule<TargetJvmVersionRule>("com.android.tools.build:builder") { params(javaVersion) }
-			//withModule<TargetJvmVersionRule>("com.android.tools.build:builder-model") { params(javaVersion) }
-			//withModule<TargetJvmVersionRule>("com.android.tools.build:builder-test-api") { params(javaVersion) }
-			//withModule<TargetJvmVersionRule>("com.android.tools.build:manifest-merger") { params(javaVersion) }
-			//withModule<TargetJvmVersionRule>("com.android.tools.build:aapt2-proto") { params(javaVersion) }
-			//withModule<TargetJvmVersionRule>("com.android.tools.build:aaptcompiler") { params(javaVersion) }
+			withModule<TargetJvmVersionRule>("com.android.tools.build:gradle") { params(javaVersion) }
+			withModule<TargetJvmVersionRule>("com.android.tools.build:gradle-api") { params(javaVersion) }
+			withModule<TargetJvmVersionRule>("com.android.tools.build:gradle-settings-api") { params(javaVersion) }
+			withModule<TargetJvmVersionRule>("com.android.tools.build:builder") { params(javaVersion) }
+			withModule<TargetJvmVersionRule>("com.android.tools.build:builder-model") { params(javaVersion) }
+			withModule<TargetJvmVersionRule>("com.android.tools.build:builder-test-api") { params(javaVersion) }
+			withModule<TargetJvmVersionRule>("com.android.tools.build:manifest-merger") { params(javaVersion) }
+			withModule<TargetJvmVersionRule>("com.android.tools.build:aapt2-proto") { params(javaVersion) }
+			withModule<TargetJvmVersionRule>("com.android.tools.build:aaptcompiler") { params(javaVersion) }
+		}
+	}
+}
+
+@CacheableRule
+abstract class TargetJvmVersionRule @Inject constructor(
+	private val jvmVersionOverride: Int
+) : ComponentMetadataRule {
+
+	override fun execute(context: ComponentMetadataContext) {
+		context.details.withVariant("runtimeElements") {
+			val version = context.details.id.version
+			if (version.startsWith("8.2.") || version.startsWith("31.2.")) {
+				attributes {
+					val original = getAttribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE)!!
+					if (jvmVersionOverride < original) {
+						attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, jvmVersionOverride)
+					} else {
+						error("It is no longer necessary to override to ${jvmVersionOverride} for ${context.details.id}")
+					}
+				}
+			}
 		}
 	}
 }
