cdec266152a8cb1b5c491e3becc1a11ed844fcc9
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-06-05 16:18:04 +0100

    Rewrite VersionsTask to be configuration cache friendly. So no direct reading of Project during @TaskAction.

--- quality/src/main/kotlin/net/twisterrob/gradle/quality/tasks/VersionsTask.kt
+++ quality/src/main/kotlin/net/twisterrob/gradle/quality/tasks/VersionsTask.kt
@@ -1,9 +1,12 @@
 package net.twisterrob.gradle.quality.tasks
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.Project
 import org.gradle.api.plugins.quality.CheckstyleExtension
 import org.gradle.api.plugins.quality.CodeQualityExtension
 import org.gradle.api.plugins.quality.PmdExtension
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.UntrackedTask
 
@@ -11,21 +14,33 @@ import org.gradle.api.tasks.UntrackedTask
 @UntrackedTask(because = "It is used to inspect Gradle state, output is console.")
 abstract class VersionsTask : DefaultTask() {
 
+	@get:Input
+	internal abstract val gradleVersion: Property<String>
+
+	@get:Input
+	internal abstract val checkstyleVersion: Property<String>
+
+	@get:Input
+	internal abstract val pmdVersion: Property<String>
+
 	init {
 		outputs.upToDateWhen { false }
+		gradleVersion.convention(project.provider { project.gradle.gradleVersion })
+		checkstyleVersion.convention(project.provider { project.getVersion<CheckstyleExtension>("checkstyle") })
+		pmdVersion.convention(project.provider { project.getVersion<PmdExtension>("pmd") })
 	}
 
 	@TaskAction
 	fun printVersions() {
 		logger.quiet(
 			"""
-				Gradle version: ${project.gradle.gradleVersion}
-				Checkstyle version: ${getVersion("checkstyle", CheckstyleExtension::class.java)}
-				PMD version: ${getVersion("pmd", PmdExtension::class.java)}
+				Gradle version: ${gradleVersion.get()}
+				Checkstyle version: ${checkstyleVersion.get()}
+				PMD version: ${pmdVersion.get()}
 			""".trimIndent()
 		)
 	}
-
-	private fun getVersion(pluginName: String, type: Class<out CodeQualityExtension>): String =
-		project.extensions.findByType(type)?.toolVersion ?: "'${pluginName}' plugin not applied"
 }
+
+private inline fun <reified T : CodeQualityExtension> Project.getVersion(pluginName: String): String =
+	project.extensions.findByType(T::class.java)?.toolVersion ?: "'${pluginName}' plugin not applied"
