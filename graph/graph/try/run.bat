61f73f893bcedc62824749102e2e4c48a5072c70
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-06-07 15:49:41 +0100

    Compose build so dependencies work correctly.

--- graph/graph/try/run.bat
+++ graph/graph/try/run.bat
@@ -1,1 +1,1 @@
-..\..\gradlew.bat -p . --stacktrace --continue --no-daemon -x lint :testR :aR --exclude-task compileRShaders %* 
+..\..\gradlew.bat -p . --continue --no-daemon -x lint :testR :aR --exclude-task compileRShaders %* 
