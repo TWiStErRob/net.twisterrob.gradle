cbd789878c094d44f253623baa92e9baaee1d347
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-06-08 10:09:36 +0100

    Convert Groovy to Java

--- graph/src/main/groovy/net/twisterrob/gradle/graph/vis/d3/javafx/D3JavaFXTaskVisualizer.java
+++ graph/src/main/groovy/net/twisterrob/gradle/graph/vis/d3/javafx/D3JavaFXTaskVisualizer.java
@@ -1,12 +1,14 @@
-package net.twisterrob.gradle.graph.vis.d3.javafx
+package net.twisterrob.gradle.graph.vis.d3.javafx;
 
-import javafx.event.EventHandler
-import javafx.stage.*
-import net.twisterrob.gradle.graph.tasks.*
-import net.twisterrob.gradle.graph.vis.TaskVisualizer
-import net.twisterrob.gradle.graph.vis.d3.javafx.Settings.WindowLocation
-import org.gradle.api.*
-import org.gradle.cache.PersistentCache
+import java.util.Map;
+
+import javafx.event.EventHandler;
+import javafx.stage.*;
+import net.twisterrob.gradle.graph.tasks.*;
+import net.twisterrob.gradle.graph.vis.TaskVisualizer;
+import net.twisterrob.gradle.graph.vis.d3.javafx.Settings.WindowLocation;
+import org.gradle.api.*;
+import org.gradle.cache.PersistentCache;
 
 /**
  * @see <a href="http://stackoverflow.com/a/20125944/253468">Idea from SO</a>
@@ -25,20 +27,24 @@ public class D3JavaFXTaskVisualizer implements TaskVisualizer {
 		if (window != null) {
 			settings.getSettings().applyTo(window.getStage());
 			window.getStage().setOnHiding(new EventHandler<WindowEvent>() {
-				@Override void handle(WindowEvent event) {
-					settings.setSettings(new WindowLocation((Window)event.source));
+				@Override public void handle(WindowEvent event) {
+					settings.setSettings(new WindowLocation((Window)event.getSource()));
 					settings.close();
 				}
-			})
+			});
 		}
 	}
 
 	@Override public void initModel(Map<Task, TaskData> graph) {
-		window?.initModel(graph);
+		if (window != null) {
+			window.initModel(graph);
+		}
 	}
 
 	@Override public void update(Task task, TaskResult result) {
-		window?.update(task, result);
+		if (window != null) {
+			window.update(task, result);
+		}
 	}
 
 	@Override public void closeUI() {
