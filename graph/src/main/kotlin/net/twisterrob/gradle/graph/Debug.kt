4d635cf469b65321470bf7809ffa83dae380e6fa
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-06-12 15:10:32 +0100

    Fix task `:test:generateNet.twisterrob.gradle.testToNet.twisterrob.gradle.plugin.gradle.testDeprecationPluginResources` not being addressable in model.update.

--- graph/src/main/kotlin/net/twisterrob/gradle/graph/Debug.kt
+++ graph/src/main/kotlin/net/twisterrob/gradle/graph/Debug.kt
@@ -4,9 +4,9 @@ object Debug {
 	/**
 	 * Setting this to the right value (fully qualified path of the project) enables clickable links from [WebView] logs.
 	 */
-	const val ResourcesFolder: String = """P:\projects\workspace\net.twisterrob.gradle\graph\src\main\resources\"""
+	const val ResourcesFolder: String = """"""
 	const val JavaFx: Boolean = false
 	const val WebView: Boolean = true
 	const val JavaScriptInterop: Boolean = false
-	const val Gradle: Boolean = false
+	const val Gradle: Boolean = true
 }
