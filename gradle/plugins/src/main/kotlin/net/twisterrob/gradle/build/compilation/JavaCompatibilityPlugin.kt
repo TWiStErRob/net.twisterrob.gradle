433440f5aa034dee35a0ea2ea6573e321ad12937
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-06-15 10:12:08 +0100

    Add note in code

--- gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/compilation/JavaCompatibilityPlugin.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/compilation/JavaCompatibilityPlugin.kt
@@ -1,16 +1,14 @@
 package net.twisterrob.gradle.build.compilation
 
-//import net.twisterrob.gradle.build.dsl.libs
-//import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-//import org.gradle.kotlin.dsl.withModule
 
 class JavaCompatibilityPlugin : Plugin<Project> {
 	override fun apply(target: Project) {
 		target.dependencies.components {
 			// AGP 8.0.0-alpha10 requires Java 17 (https://issuetracker.google.com/issues/241546506)
 			// But Google didn't change the Java compiler and class file format to be Java 17 yet.
+			// (There are plans: https://issuetracker.google.com/issues/274742629)
 			// Even AGP 8.1.0-beta03 is published with Java 11 bytecode
 			// according to its gradle-module-metadata and class header.
 			// AGP 8.2.0-alpha03 to AGP 8.2.0-alpha07 is published with Java 17 bytecode according to its metadata.
