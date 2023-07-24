7449d4154b9d46c8c79cd1033770550249735048
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-07-24 22:25:33 +0100

    Fix tests for all versions

--- plugin/settings/src/test/kotlin/net/twisterrob/gradle/GradleUtilsIntgTest_doNotNagAbout.kt
+++ plugin/settings/src/test/kotlin/net/twisterrob/gradle/GradleUtilsIntgTest_doNotNagAbout.kt
@@ -196,14 +196,8 @@ class GradleUtilsIntgTest_doNotNagAbout : BaseIntgTest() {
 	}
 
 	private fun nagManyTimes(): String =
+		@Suppress("UseIfInsteadOfWhen")
 		when {
-			GradleVersion.version("8.0") <= gradle.gradleVersion.baseVersion -> {
-				"""
-					// Uses Conventions which are deprecated and nag.
-					// Uses BasePluginConvention.setArchivesBaseName(String) which is deprecated and nags.
-					archivesBaseName = 'trigger-nagging' // line 6
-				""".trimIndent()
-			}
 			GradleVersion.version("7.0") <= gradle.gradleVersion.baseVersion -> {
 				"""
 					repositories.jcenter()
@@ -218,22 +212,37 @@ class GradleUtilsIntgTest_doNotNagAbout : BaseIntgTest() {
 		when {
 			GradleVersion.version("8.0") <= gradle.gradleVersion.baseVersion -> {
 				assertHasOutputLine(
-					"The org.gradle.api.plugins.BasePluginConvention type has been deprecated. " +
+					"The RepositoryHandler.jcenter() method has been deprecated. " +
 							"This is scheduled to be removed in Gradle 9.0. " +
+							"JFrog announced JCenter's sunset in February 2021. " +
+							"Use mavenCentral() instead. " +
 							"Consult the upgrading guide for further information: " +
-							"https://docs.gradle.org/${gradle.gradleVersion.version}/userguide/upgrading_version_8.html" +
-							"#base_convention_deprecation"
+							"https://docs.gradle.org/${gradle.gradleVersion.version}/userguide/upgrading_version_6.html" +
+							"#jcenter_deprecation"
 				)
-				assertHasOutputLine(Regex("""Build file '\Q${gradle.buildFile.absolutePath}\E': line 6"""))
-				assertHasOutputLine(Regex("""\tat build_[a-z0-9]+\.run\(\Q${gradle.buildFile.absolutePath}\E:6\)"""))
+				assertHasOutputLine(Regex("""\tat build_[a-z0-9]+\.run\(\Q${gradle.buildFile.absolutePath}\E:4\)"""))
+			}
+			GradleVersion.version("7.2") <= gradle.gradleVersion.baseVersion -> {
+				assertHasOutputLine(
+					"The RepositoryHandler.jcenter() method has been deprecated. " +
+							"This is scheduled to be removed in Gradle 8.0. " +
+							"JFrog announced JCenter's sunset in February 2021. " +
+							"Use mavenCentral() instead. " +
+							"Consult the upgrading guide for further information: " +
+							"https://docs.gradle.org/${gradle.gradleVersion.version}/userguide/upgrading_version_6.html" +
+							"#jcenter_deprecation"
+				)
+				assertHasOutputLine(Regex("""\tat build_[a-z0-9]+\.run\(\Q${gradle.buildFile.absolutePath}\E:4\)"""))
 			}
 			GradleVersion.version("7.0") <= gradle.gradleVersion.baseVersion -> {
 				assertHasOutputLine(
-					"Internal API constructor DefaultDomainObjectSet(Class<T>) has been deprecated. " +
+					"The RepositoryHandler.jcenter() method has been deprecated. " +
 							"This is scheduled to be removed in Gradle 8.0. " +
-							"Please use ObjectFactory.domainObjectSet(Class<T>) instead. " +
-							"See https://docs.gradle.org/7.0.2/userguide/custom_gradle_types.html#domainobjectset " +
-							"for more details."
+							"JFrog announced JCenter's shutdown in February 2021. " +
+							"Use mavenCentral() instead. " +
+							"Consult the upgrading guide for further information: " +
+							"https://docs.gradle.org/${gradle.gradleVersion.version}/userguide/upgrading_version_6.html" +
+							"#jcenter_deprecation"
 				)
 				assertHasOutputLine(Regex("""\tat build_[a-z0-9]+\.run\(\Q${gradle.buildFile.absolutePath}\E:4\)"""))
 			}
@@ -249,11 +258,13 @@ class GradleUtilsIntgTest_doNotNagAbout : BaseIntgTest() {
 				"""
 					// ClosureBackedAction type is deprecated, nagging is in the class initializer.
 					//noinspection GrDeprecatedAPIUsage,UnnecessaryQualifiedReference
-					org.gradle.util.ClosureBackedAction.of {} // line 10
+					org.gradle.util.ClosureBackedAction.of {}
 				""".trimIndent()
 			}
 			GradleVersion.version("7.0") <= gradle.gradleVersion.baseVersion -> {
 				"""
+					// DefaultDomainObjectSet constructor taking a class is deprecated, nagging is in the constructor.
+					//noinspection GrDeprecatedAPIUsage,UnnecessaryQualifiedReference
 					new org.gradle.api.internal.DefaultDomainObjectSet(String.class)
 				""".trimIndent()
 			}
@@ -272,19 +283,17 @@ class GradleUtilsIntgTest_doNotNagAbout : BaseIntgTest() {
 							"https://docs.gradle.org/${gradle.gradleVersion.version}/userguide/upgrading_version_7.html" +
 							"#org_gradle_util_reports_deprecations"
 				)
-				assertHasOutputLine(Regex("""Build file '\Q${gradle.buildFile.absolutePath}\E': line 10"""))
-				assertHasOutputLine(Regex("""\tat build_[a-z0-9]+\.run\(\Q${gradle.buildFile.absolutePath}\E:10\)"""))
+				assertHasOutputLine(Regex("""\tat build_[a-z0-9]+\.run\(\Q${gradle.buildFile.absolutePath}\E:8\)"""))
 			}
 			GradleVersion.version("7.0") <= gradle.gradleVersion.baseVersion -> {
 				assertHasOutputLine(
-					"The RepositoryHandler.jcenter() method has been deprecated. " +
+					"Internal API constructor DefaultDomainObjectSet(Class<T>) has been deprecated. " +
 							"This is scheduled to be removed in Gradle 8.0. " +
-							"JFrog announced JCenter's shutdown in February 2021. " +
-							"Use mavenCentral() instead. " +
-							"Consult the upgrading guide for further information: " +
-							"https://docs.gradle.org/7.0.2/userguide/upgrading_version_6.html#jcenter_deprecation"
+							"Please use ObjectFactory.domainObjectSet(Class<T>) instead. " +
+							"See https://docs.gradle.org/${gradle.gradleVersion.version}/userguide/custom_gradle_types.html" +
+							"#domainobjectset for more details."
 				)
-				assertHasOutputLine(Regex("""\tat build_[a-z0-9]+\.run\(\Q${gradle.buildFile.absolutePath}\E:6\)"""))
+				assertHasOutputLine(Regex("""\tat build_[a-z0-9]+\.run\(\Q${gradle.buildFile.absolutePath}\E:8\)"""))
 			}
 			else -> {
 				error("Cannot nag once for ${gradle.gradleVersion}")
