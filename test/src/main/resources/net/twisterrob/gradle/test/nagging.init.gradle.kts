5d4222fe73a43b101aa6b2b793ee1c8d05ace00b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-04-10 09:06:04 +0100

    Comments: extract tests explicitly

--- test/src/main/resources/net/twisterrob/gradle/test/nagging.init.gradle.kts
+++ test/src/main/resources/net/twisterrob/gradle/test/nagging.init.gradle.kts
@@ -65,7 +65,7 @@ doNotNagAboutStackForTest(
 	"8.0" to "8.7",
 	"7.4" to "8.2",
 	// > Task :checkDebugUnitTestAarMetadata, :mergeDebugUnitTestResources, :processDebugUnitTestManifest, :mergeDebugUnitTestAssets
-	// in `AndroidBuildPluginIntgTest.can disable buildConfig decoration (debug)` for example.
+	// Example test: AndroidBuildPluginIntgTest.`can disable buildConfig decoration (debug)`
 	// Ignore warning for https://issuetracker.google.com/issues/279306626, it'll be fixed when AGP's minimum is Gradle 8.2 (8.2.0-alpha13).
 	"The BuildIdentifier.getName() method has been deprecated. This is scheduled to be removed in Gradle 9.0. Use getBuildPath() to get a unique identifier for the build. Consult the upgrading guide for further information: https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#build_identifier_name_and_current_deprecation",
 	"at com.android.build.gradle.internal.ide.dependencies.BuildMappingUtils.getIdString(BuildMapping.kt:48)"
@@ -74,7 +74,8 @@ doNotNagAboutStackForTest(
 	"8.0" to "8.7",
 	"7.4" to "8.2",
 	// > Task :generateDebugLintModel. :lintAnalyzeDebug, :lintReportDebug
-	// > Task :lintAnalyzeDebug in HtmlReportTaskTest.task is re-executed when lint results are changed
+	// > Task :lintAnalyzeDebug
+	// Example test: HtmlReportTaskTest.`task is re-executed when lint results are changed`
 	// Ignore warning for https://issuetracker.google.com/issues/279306626, it'll be fixed when AGP's minimum is Gradle 8.2 (8.2.0-alpha13).
 	"The BuildIdentifier.isCurrentBuild() method has been deprecated. This is scheduled to be removed in Gradle 9.0. Use getBuildPath() to get a unique identifier for the build. Consult the upgrading guide for further information: https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#build_identifier_name_and_current_deprecation",
 	"at com.android.build.gradle.internal.ide.dependencies.BuildMappingUtils.getBuildId(BuildMapping.kt:40)"
@@ -82,7 +83,8 @@ doNotNagAboutStackForTest(
 doNotNagAboutStackForTest(
 	"8.0" to "8.7",
 	"7.4" to "8.0",
-	// > Task :lintAnalyzeDebug in HtmlReportTaskTest.task is re-executed when lint results are changed
+	// > Task :lintAnalyzeDebug
+	// Example test: HtmlReportTaskTest.`task is re-executed when lint results are changed`
 	"The BuildIdentifier.isCurrentBuild() method has been deprecated. This is scheduled to be removed in Gradle 9.0. Use getBuildPath() to get a unique identifier for the build. Consult the upgrading guide for further information: https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#build_identifier_name_and_current_deprecation",
 	"at com.android.build.gradle.internal.dependency.ConstraintHandler\$alignWith\$1\$1.execute(ConstraintHandler.kt:68)"
 )
@@ -96,14 +98,16 @@ doNotNagAboutStackForTest(
 doNotNagAboutStackForTest(
 	"8.3" to "8.8",
 	"7.4" to "8.0",
-	// > Configure project : in TestReportGeneratorIntgTest
+	// > Configure project :
+	// Example test: TestReportGeneratorIntgTest
 	"The org.gradle.util.GUtil type has been deprecated. This is scheduled to be removed in Gradle 9.0. Consult the upgrading guide for further information: https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_7.html#org_gradle_util_reports_deprecations",
 	"at com.android.build.gradle.internal.api.DefaultAndroidSourceSet.<init>(DefaultAndroidSourceSet.kt:68)"
 )
 doNotNagAboutStackForTest(
 	"8.7" to "8.8",
 	"7.4" to "8.0",
-	// > Configure project : in all Android tests (e.g. AndroidBuildPluginIntgTest#can override compileSdk (debug))
+	// > Configure project : in all Android tests
+	// Example test: AndroidBuildPluginIntgTest`can override compileSdk (debug)`
 	"Declaring client module dependencies has been deprecated. This is scheduled to be removed in Gradle 9.0. Please use component metadata rules instead. Consult the upgrading guide for further information: https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#declaring_client_module_dependencies",
 	"at com.android.build.gradle.internal.res.Aapt2FromMaven\$Companion.create(Aapt2FromMaven.kt:138)"
 )
@@ -125,8 +129,9 @@ if ("@net.twisterrob.test.kotlin.pluginVersion@" < "1.6.0") {
 
 if ("@net.twisterrob.test.kotlin.pluginVersion@" < "1.7.0") {
 	// https://youtrack.jetbrains.com/issue/KT-32805#focus=Comments-27-5915479.0-0 was fixed in 1.7.0.
-	// > Task :kaptGenerateStubsDebugKotlin in KotlinPluginIntgTest.can test kotlin with JUnit in Android Test App
+	// > Task :kaptGenerateStubsDebugKotlin
 	// > Task :kaptGenerateStubsDebugUnitTestKotlin
+	// Example test: KotlinPluginIntgTest.`can test kotlin with JUnit in Android Test App`
 	doNotNagAboutStackForTest(
 		"8.0" to "8.8",
 		"0.0" to "100.0",
@@ -156,7 +161,8 @@ if ("@net.twisterrob.test.kotlin.pluginVersion@" < "1.7.0") {
 if ("@net.twisterrob.test.kotlin.pluginVersion@" < "1.7.20") {
 	// https://youtrack.jetbrains.com/issue/KT-53882
 	// https://youtrack.jetbrains.com/issue/KT-57908
-	// > Configure project : in KotlinPluginIntgTest#can test kotlin with JUnit in Android Test App
+	// > Configure project :
+	// Example test: KotlinPluginIntgTest.`can test kotlin with JUnit in Android Test App`
 	doNotNagAboutStackForTest(
 		"8.0" to "8.8",
 		"0.0" to "100.0",
@@ -186,7 +192,8 @@ if ("@net.twisterrob.test.kotlin.pluginVersion@" < "1.7.20") {
 
 if ("@net.twisterrob.test.kotlin.pluginVersion@" < "1.7.0") {
 	// https://github.com/JetBrains/kotlin/commit/bc8f795f71ed6cbc05fc542c6a32f26da003d718
-	// > Task :test:kaptGenerateStubsDebugKotlin in KotlinPluginIntgTest.can test kotlin with JUnit in Android Test App
+	// > Task :test:kaptGenerateStubsDebugKotlin
+	// Example test: KotlinPluginIntgTest.`can test kotlin with JUnit in Android Test App`
 	doNotNagAboutStackForTest(
 		"8.0" to "8.8",
 		"0.0" to "100.0",
