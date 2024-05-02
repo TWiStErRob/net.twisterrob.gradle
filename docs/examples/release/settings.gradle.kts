ba007af85b14a0900d7d86c69158ce43e84d298c
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-05-02 02:03:58 +0100

    Remove nagging from examples

--- docs/examples/release/settings.gradle.kts
+++ docs/examples/release/settings.gradle.kts
@@ -1,5 +1,3 @@
-import net.twisterrob.gradle.doNotNagAbout
-
 pluginManagement {
 	repositories {
 		mavenCentral()
@@ -20,7 +18,6 @@ pluginManagement {
 
 plugins {
 	id("net.twisterrob.gradle.plugin.settings") version "0.17"
-	id("net.twisterrob.gradle.plugin.nagging") version "0.17"
 }
 
 dependencyResolutionManagement {
@@ -34,17 +31,3 @@ dependencyResolutionManagement {
 if (!JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_17)) {
 	error("Java 17+ is required to build this project, found: ${JavaVersion.current()}.")
 }
-
-val gradleVersion: String = GradleVersion.current().version
-
-// TODEL Gradle 8.2 vs AGP 8.0.2 https://issuetracker.google.com/issues/279306626
-// Gradle 8.2 M1 added nagging for BuildIdentifier.*, which was not replaced in AGP 8.0.x yet.
-@Suppress("detekt.MaxLineLength")
-doNotNagAbout(
-	"The BuildIdentifier.isCurrentBuild() method has been deprecated. " +
-			"This is scheduled to be removed in Gradle 9.0. " +
-			"Use getBuildPath() to get a unique identifier for the build. " +
-			"Consult the upgrading guide for further information: " +
-			"https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#build_identifier_name_and_current_deprecation",
-	"at com.android.build.gradle.internal.ide.dependencies.BuildMappingUtils.getBuildId(BuildMapping.kt:40)"
-)
