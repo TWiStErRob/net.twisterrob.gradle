f383029fe7dc7090b71b237b3473aa85bf0b4c23
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-09-14 11:12:44 +0100

    Multiversion JGit

--- plugin/versioning/build.gradle.kts
+++ plugin/versioning/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
 	implementation(projects.compat.agp)
 	implementation(libs.svnkit)
 	implementation(libs.svnkit.cli)
-	implementation(libs.jgit)
+	compileOnly(libs.jgit17)
 	compileOnly(libs.android.gradle)
 
 	// This plugin is part of the net.twisterrob.gradle.plugin.android-app plugin, not designed to work on its own.
@@ -50,5 +50,54 @@ dependencies {
 	}
 
 	testFixturesApi(libs.svnkit)
-	testFixturesApi(libs.jgit)
+	testFixturesApi(libs.jgit17)
+}
+
+val runtimeElementsJava11 by configurations.creating {
+	this.extendsFrom(configurations.runtimeElements.get())
+	val other = configurations.runtimeElements.get().attributes
+	attributes {
+		other.keySet().forEach {
+			@Suppress("UNCHECKED_CAST")
+			attribute(it as Attribute<Any>, other.getAttribute(it)!!)
+		}
+		attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 11)
+	}
+}
+
+val runtimeElementsJava17 by configurations.creating {
+	this.extendsFrom(configurations.runtimeElements.get())
+	val other = configurations.runtimeElements.get().attributes
+	attributes {
+		other.keySet().forEach {
+			@Suppress("UNCHECKED_CAST")
+			attribute(it as Attribute<Any>, other.getAttribute(it)!!)
+		}
+		attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
+	}
+}
+
+configurations.runtimeElements {
+	attributes {
+		attribute(Attribute.of("reject", String::class.java), "yes")
+	}
+}
+
+dependencies {
+	constraints {
+		runtimeElementsJava11(libs.jgit11) {
+			because("JGit 6.10.0 is the last version to support Java 11. JGit 7.x requires Java 17.")
+			versionConstraint.rejectedVersions.add("[7.0,)")
+		}
+		runtimeElementsJava17(libs.jgit17) {
+			because("JGit 7.x is the first version to require Java 17.")
+			versionConstraint.rejectedVersions.add("(,7.0)")
+		}
+	}
+}
+
+components.getByName<AdhocComponentWithVariants>("java") {
+	withVariantsFromConfiguration(configurations.runtimeElements.get()) { skip() }
+	addVariantsFromConfiguration(runtimeElementsJava11) {}
+	addVariantsFromConfiguration(runtimeElementsJava17) {}
 }
