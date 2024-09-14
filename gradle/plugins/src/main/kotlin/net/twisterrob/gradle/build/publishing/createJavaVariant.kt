c23311dca5273f706c2e778087b8aead6430e464
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-09-14 23:50:18 +0100

    Encapsulate configuration in plugins

---
+++ gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/publishing/createJavaVariant.kt
@@ -0,0 +1,60 @@
+package net.twisterrob.gradle.build.publishing
+
+import org.gradle.api.NamedDomainObjectProvider
+import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.attributes.Attribute
+import org.gradle.api.attributes.HasAttributes
+import org.gradle.api.attributes.java.TargetJvmVersion
+import org.gradle.api.component.AdhocComponentWithVariants
+import org.gradle.api.provider.ProviderFactory
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.gradle.kotlin.dsl.getByName
+import org.gradle.kotlin.dsl.named
+
+fun Project.createJavaVariant(
+	forConfiguration: NamedDomainObjectProvider<Configuration>,
+	version: JavaLanguageVersion,
+): Configuration {
+	invalidateConfiguration(forConfiguration)
+	val javaVersion = version.asInt()
+	val runtimeElementsJava = configurations.consumable("${forConfiguration.name}Java$javaVersion") {
+		val base = forConfiguration.get()
+		extendsFrom(base)
+		copyAttributesFrom(providers, base)
+		attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, javaVersion)
+	}
+
+	components.named<AdhocComponentWithVariants>("java").configure {
+		addVariantsFromConfiguration(runtimeElementsJava.get()) { }
+	}
+
+	val onlyName = forConfiguration.name.replace("Elements", "Only")
+	val runtimeOnlyJava = configurations.resolvable("${onlyName}Java$javaVersion") {
+		val base = configurations.getByName(onlyName)
+		extendsFrom(base)
+		runtimeElementsJava.get().extendsFrom(this)
+		copyAttributesFrom(providers, base)
+		attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, javaVersion)
+	}
+	return runtimeOnlyJava.get()
+}
+
+private fun HasAttributes.copyAttributesFrom(providers: ProviderFactory, origin: HasAttributes) {
+	for (key in origin.attributes.keySet()) {
+		@Suppress("UNCHECKED_CAST") // The origin will make sure it's the right type.
+		val unsafeKey = key as Attribute<Any>
+		this.attributes.attributeProvider(unsafeKey, providers.provider { origin.attributes.getAttribute(key) })
+	}
+}
+
+private fun Project.invalidateConfiguration(configuration: NamedDomainObjectProvider<Configuration>) {
+	configuration.configure {
+		attributes {
+			attribute(Attribute.of("reject", String::class.java), "yes")
+		}
+	}
+	this.components.getByName<AdhocComponentWithVariants>("java") {
+		withVariantsFromConfiguration(configuration.get()) { skip() }
+	}
+}
--- plugin/versioning/build.gradle.kts
+++ plugin/versioning/build.gradle.kts
@@ -1,3 +1,5 @@
+import net.twisterrob.gradle.build.publishing.createJavaVariant
+
 plugins {
 	id("net.twisterrob.gradle.build.module.gradle-plugin")
 	id("net.twisterrob.gradle.build.publish")
@@ -29,6 +31,9 @@ gradlePlugin {
 	}
 }
 
+val runtimeOnlyJava11: Configuration = createJavaVariant(configurations.runtimeElements, JavaLanguageVersion.of(11))
+val runtimeOnlyJava17: Configuration = createJavaVariant(configurations.runtimeElements, JavaLanguageVersion.of(17))
+
 dependencies {
 	implementation(gradleApi())
 	implementation(projects.plugin.base)
@@ -37,8 +42,18 @@ dependencies {
 	implementation(projects.compat.agp)
 	implementation(libs.svnkit)
 	implementation(libs.svnkit.cli)
-	compileOnly(libs.jgit17)
 	compileOnly(libs.android.gradle)
+	compileOnly(libs.jgit17)
+	@Suppress("UnstableApiUsage")
+	runtimeOnlyJava11(libs.jgit11) {
+		because("JGit 6.10.0 is the last version to support Java 11. JGit 7.x requires Java 17.")
+		versionConstraint.rejectedVersions.add("[7.0,)")
+	}
+	@Suppress("UnstableApiUsage")
+	runtimeOnlyJava17(libs.jgit17) {
+		because("JGit 7.x is the first version to require Java 17.")
+		versionConstraint.rejectedVersions.add("(,7.0)")
+	}
 
 	// This plugin is part of the net.twisterrob.gradle.plugin.android-app plugin, not designed to work on its own.
 	runtimeOnly(projects.plugin)
@@ -51,57 +66,4 @@ dependencies {
 
 	testFixturesApi(libs.svnkit)
 	testFixturesCompileOnlyApi(libs.jgit17)
-}
-
-fun HasAttributes.copyAttributesFrom(providers: ProviderFactory, origin: HasAttributes) {
-	for (key in origin.attributes.keySet()) {
-		@Suppress("UNCHECKED_CAST") // The origin will make sure it's the right type.
-		val unsafeKey = key as Attribute<Any>
-		this.attributes.attributeProvider(unsafeKey, providers.provider { origin.attributes.getAttribute(key) })
-	}
-}
-
-fun NamedDomainObjectProvider<Configuration>.createJavaVariant(javaVersion: Int) : Configuration {
-	val runtimeElementsJava = configurations.consumable("runtimeElementsJava$javaVersion") {
-		val base = get()
-		extendsFrom(base)
-		copyAttributesFrom(providers, base)
-		attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, javaVersion)
-	}
-
-	components.named<AdhocComponentWithVariants>("java").configure {
-		addVariantsFromConfiguration(runtimeElementsJava.get()) { }
-	}
-
-	val runtimeOnlyJava = configurations.resolvable("runtimeOnlyJava$javaVersion") {
-		val base = configurations.getByName(get().name.replace("Elements", "Only"))
-		extendsFrom(base)
-		runtimeElementsJava.get().extendsFrom(this)
-		copyAttributesFrom(providers, base)
-		attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, javaVersion)
-	}
-	return runtimeOnlyJava.get()
-}
-
-val runtimeOnlyJava11 = configurations.runtimeElements.createJavaVariant(11)
-val runtimeOnlyJava17 = configurations.runtimeElements.createJavaVariant(17)
-
-configurations.runtimeElements {
-	attributes {
-		attribute(Attribute.of("reject", String::class.java), "yes")
-	}
-}
-components.getByName<AdhocComponentWithVariants>("java") {
-	withVariantsFromConfiguration(configurations.runtimeElements.get()) { skip() }
-}
-
-dependencies {
-	runtimeOnlyJava11(libs.jgit11) {
-		because("JGit 6.10.0 is the last version to support Java 11. JGit 7.x requires Java 17.")
-		versionConstraint.rejectedVersions.add("[7.0,)")
-	}
-	runtimeOnlyJava17(libs.jgit17) {
-		because("JGit 7.x is the first version to require Java 17.")
-		versionConstraint.rejectedVersions.add("(,7.0)")
-	}
 }
