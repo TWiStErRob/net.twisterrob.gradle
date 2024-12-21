45730f7df0177acbf28ec24989235bd5edbe45c0
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-12-21 11:57:14 +0000

    Move suppression to :graph

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -166,16 +166,3 @@ if ((System.getProperty("idea.version") ?: "") < "2023.3") {
 	val error: (String) -> Unit = if (isCI) ::error else logger::warn
 	error("IDEA version changed, please review hack.")
 }
-
-// TODEL Kover >0.9.0 https://github.com/Kotlin/kotlinx-kover/issues/716
-@Suppress("detekt.MaxLineLength", "detekt.StringLiteralDuplication")
-doNotNagAbout(
-	"Calling configuration method 'attributes(Action)' is deprecated for configuration 'kover', which has permitted usage(s):\n" +
-			"\tDeclarable - this configuration can have dependencies added to it\n" +
-			"This method is only meant to be called on configurations which allow the (non-deprecated) usage(s): 'Consumable, Resolvable'. " +
-			"This behavior has been deprecated. " +
-			"This behavior is scheduled to be removed in Gradle 9.0. " +
-			"Consult the upgrading guide for further information: " +
-			"https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#deprecated_configuration_usage",
-	"at kotlinx.kover.gradle.plugin.appliers.PrepareKoverKt\$prepare\$koverBucketConfiguration\$1.execute(PrepareKover.kt:26)",
-)
