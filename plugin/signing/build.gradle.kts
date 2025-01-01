f61cc47c36593e7cd2bc6e17f133c60932313870
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-01-01 19:59:33 +0000

    Run gradlew :plugin:signing:test with toolchain

--- plugin/signing/build.gradle.kts
+++ plugin/signing/build.gradle.kts
@@ -28,3 +28,9 @@ dependencies {
 		version { require(property("net.twisterrob.test.android.pluginVersion").toString()) }
 	}
 }
+
+tasks.withType<Test>().configureEach {
+	javaLauncher = javaToolchains.launcherFor {
+		languageVersion = JavaLanguageVersion.of(11)
+	}
+}
