b0cfe0375dc07337779c64375937ba457838c9fe
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-01-04 02:06:59 +0000

    Update warnings

--- test/src/main/resources/net/twisterrob/gradle/test/nagging.init.gradle.kts
+++ test/src/main/resources/net/twisterrob/gradle/test/nagging.init.gradle.kts
@@ -62,7 +62,7 @@ doNotNagAboutStackForTest(
 	"at com.android.build.gradle.tasks.factory.AndroidUnitTest\$CreationAction.configure"
 )
 doNotNagAboutStackForTest(
-	"8.0" to "8.11",
+	"8.0" to "8.12",
 	"7.4" to "8.2",
 	// > Task :checkDebugUnitTestAarMetadata, :mergeDebugUnitTestResources, :processDebugUnitTestManifest, :mergeDebugUnitTestAssets
 	// Example test: AndroidBuildPluginIntgTest.`can disable buildConfig decoration (debug)`
@@ -71,7 +71,7 @@ doNotNagAboutStackForTest(
 	"at com.android.build.gradle.internal.ide.dependencies.BuildMappingUtils.getIdString(BuildMapping.kt:48)"
 )
 doNotNagAboutStackForTest(
-	"8.0" to "8.11",
+	"8.0" to "8.12",
 	"7.4" to "8.2",
 	// > Task :generateDebugLintModel. :lintAnalyzeDebug, :lintReportDebug
 	// > Task :lintAnalyzeDebug
@@ -81,7 +81,7 @@ doNotNagAboutStackForTest(
 	"at com.android.build.gradle.internal.ide.dependencies.BuildMappingUtils.getBuildId(BuildMapping.kt:40)"
 )
 doNotNagAboutStackForTest(
-	"8.0" to "8.11",
+	"8.0" to "8.12",
 	"7.4" to "8.0",
 	// > Task :lintAnalyzeDebug
 	// Example test: HtmlReportTaskTest.`task is re-executed when lint results are changed`
@@ -89,7 +89,7 @@ doNotNagAboutStackForTest(
 	"at com.android.build.gradle.internal.dependency.ConstraintHandler\$alignWith\$1\$1.execute(ConstraintHandler.kt:68)"
 )
 doNotNagAboutStackForTest(
-	"8.2" to "8.11",
+	"8.2" to "8.12",
 	"7.4" to "8.0",
 	// > Configure project : when using android.testOptions.unitTests.all { }
 	// Example test: AndroidBuildPluginIntgTest.`can disable buildConfig decoration (debug)`
@@ -97,7 +97,7 @@ doNotNagAboutStackForTest(
 	"at com.android.build.gradle.internal.dsl.TestOptions\$UnitTestOptions\$all\$1.execute(TestOptions.kt:115)"
 )
 doNotNagAboutStackForTest(
-	"8.3" to "8.11",
+	"8.3" to "8.12",
 	"7.4" to "8.0",
 	// > Configure project :
 	// Example test: TestReportGeneratorIntgTest
@@ -106,7 +106,7 @@ doNotNagAboutStackForTest(
 	"at com.android.build.gradle.internal.api.DefaultAndroidSourceSet.<init>(DefaultAndroidSourceSet.kt:68)"
 )
 doNotNagAboutStackForTest(
-	"8.7" to "8.11",
+	"8.7" to "8.12",
 	"7.4" to "8.2",
 	// > Configure project : in all Android tests
 	// Example test: AndroidBuildPluginIntgTest.`can override compileSdk (debug)`
@@ -115,7 +115,7 @@ doNotNagAboutStackForTest(
 	"at com.android.build.gradle.internal.res.Aapt2FromMaven\$Companion.create(Aapt2FromMaven.kt:13"
 )
 doNotNagAboutPatternForTest(
-	"8.8" to "8.11",
+	"8.8" to "8.12",
 	"7.4" to "8.3",
 	// > Task :generateDebugRFile
 	// > Task :generateReleaseRFile
