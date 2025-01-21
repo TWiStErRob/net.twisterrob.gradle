9cb786990d287036bb85d391c6305091325cb543
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-01-21 23:32:19 +0000

    Disable GradleDependency

--- gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/lint/LintPlugin.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/gradle/build/lint/LintPlugin.kt
@@ -7,7 +7,7 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 
 class LintPlugin : Plugin<Project> {
-	
+
 	override fun apply(project: Project) {
 		project.pluginManager.apply("com.android.lint")
 		project.dependencies.add("lintChecks", project.libs.androidx.lint)
@@ -19,6 +19,12 @@ class LintPlugin : Plugin<Project> {
 			lintConfig = rootDir.file("config/lint/lint.xml").asFile
 			val slug = project.path.replace(":", "-")
 			baseline = rootDir.file("config/lint/lint-baseline${slug}.xml").asFile
+			disable.addAll(
+				listOf(
+					// Project is using renovate to manage dependencies.
+					"GradleDependency",
+				)
+			)
 		}
 	}
 }
