d66eff7b638859b1d6cf76403664b4c8f1e4c9d6
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-01-24 14:11:46 +0000

    Revert "Classpath"
    This reverts commit b8cd539d152cd911259ce043af6d1d163e60cfe8.

--- plugin/building/build.gradle.kts
+++ plugin/building/build.gradle.kts
@@ -23,14 +23,14 @@ dependencies {
 	compileOnly(libs.android.tools.common)
 	compileOnly(libs.annotations.jetbrains)
 
+	// This plugin is part of the net.twisterrob.gradle.plugin.android-app plugin, not designed to work on its own.
+	runtimeOnly(projects.plugin)
+
 	testImplementation(projects.test.internal)
 	testImplementation(testFixtures(projects.plugin.base))
 	testImplementation(testFixtures(projects.plugin.versioning))
 	// AndroidInstallRunnerTaskTest calls production code directly, so need com.android.xml.AndroidXPathFactory.
 	testRuntimeOnly(libs.android.tools.common)
-
-	// This plugin is part of the net.twisterrob.gradle.plugin.android-app plugin, not designed to work on its own.
-	testInjectedPluginClasspath(projects.plugin)
 	testInjectedPluginClasspath(libs.android.gradle) {
 		version { require(property("net.twisterrob.test.android.pluginVersion").toString()) }
 	}
