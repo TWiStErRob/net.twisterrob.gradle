36760e00332e918291f2b8d96a2151b8db978195
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-04-21 23:17:53 +0100

    Delete old debug projects

--- docs/debug/agp70-gradle70/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-	id("com.android.application") version "7.0.4"
-}
-
-android {
-	namespace = "test.project"
-	compileSdk = 30
-}
--- docs/debug/agp74-gradle80/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-	id("com.android.application") version "7.4.2"
-}
-
-android {
-	namespace = "test.project"
-	compileSdk = 34
-}
