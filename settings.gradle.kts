2ae7977ea6e33389d26dfaf2f775f6daed27d0a7
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-09-14 23:05:01 +0100

    More nagging for host Gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -123,3 +123,20 @@ doNotNagAbout(
 			"https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_9.html#reporting_extension_file",
 	"at io.gitlab.arturbosch.detekt.DetektPlugin.apply(DetektPlugin.kt:28)",
 )
+
+// TODEL Gradle 9.1 vs AGP 8.13 https://issuetracker.google.com/issues/444260628
+@Suppress("detekt.MaxLineLength")
+doNotNagAbout(
+	Regex(
+		"Declaring dependencies using multi-string notation has been deprecated. ".escape() +
+				"This will fail with an error in Gradle 10. ".escape() +
+				"Please use single-string notation instead: ".escape() +
+				"\"${"com.android.tools.lint:lint-gradle:".escape()}\\d+\\.\\d+\\.\\d+(-(alpha|beta|rc)\\d+)?${"\". ".escape()}" +
+				"Consult the upgrading guide for further information: ".escape() +
+				"https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_9.html#dependency_multi_string_notation".escape() +
+				".*",
+	),
+	//"at com.android.build.gradle.internal.lint.LintFromMaven\$Companion.from(AndroidLintInputs.kt:2850)",
+)
+
+private fun String.escape(): String = Regex.escape(this)
