444af657f1bc2acd9f53cd3d60edeaf5b4c9b7f0
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-01-16 19:08:32 +0000

    More coverage on TableGenerator

--- quality/src/test/kotlin/net/twisterrob/gradle/quality/report/TableGeneratorTest.kt
+++ quality/src/test/kotlin/net/twisterrob/gradle/quality/report/TableGeneratorTest.kt
@@ -2,6 +2,9 @@ package net.twisterrob.gradle.quality.report
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class TableGeneratorTest {
 
@@ -25,7 +28,19 @@ class TableGeneratorTest {
 			),
 			"module3" to mapOf(
 				"variant2" to emptyMap()
-			)
+			),
+			"module4" to mapOf(
+				"variant1" to mapOf(
+					"checkstyle" to null,
+					"pmd" to null,
+				)
+			),
+		)
+
+		val NO_COUNTS: Map<String, Map<String, Map<String, Int?>>> = mapOf(
+			"module1" to mapOf("variant1" to mapOf("checker" to null)),
+			"module2" to mapOf("variant2" to mapOf("checker" to null)),
+			"module3" to mapOf("variant3" to mapOf("checker" to null)),
 		)
 	}
 
@@ -41,6 +56,7 @@ class TableGeneratorTest {
 			module2	variant2 	       N/A	N/A	      N/A
 			module2	variant3 	         0	N/A	      N/A
 			module3	variant2 	       N/A	N/A	      N/A
+			module4	variant1 	       N/A	N/A	      N/A
 			Summary	(total: 6)	         1	  5	      N/A
 		""".trimIndent().normalizeLineEndings()
 		assertEquals(expected, result)
@@ -73,6 +89,7 @@ class TableGeneratorTest {
 			module2	variant2 	       N/A	N/A
 			module2	variant3 	         0	N/A
 			module3	variant2 	       N/A	N/A
+			module4	variant1 	       N/A	N/A
 			Summary	(total: 6)	         1	  5
 		""".trimIndent().normalizeLineEndings()
 		assertEquals(expected, result)
@@ -93,8 +110,14 @@ class TableGeneratorTest {
 		assertEquals(expected, result)
 	}
 
-	@Test fun `empty data (no modules)`() {
-		val sut = TableGenerator()
+	@CsvSource(
+		"true, true",
+		"true, false",
+		"false, true",
+		"false, false",
+	)
+	@ParameterizedTest fun `empty data (no modules)`(isPrintEmptyColumns: Boolean, isPrintEmptyRows: Boolean) {
+		val sut = TableGenerator(isPrintEmptyColumns = isPrintEmptyColumns, isPrintEmptyRows = isPrintEmptyRows)
 
 		val result = sut.build(emptyMap())
 
@@ -105,8 +128,14 @@ class TableGeneratorTest {
 		assertEquals(expected, result)
 	}
 
-	@Test fun `empty data (no variants)`() {
-		val sut = TableGenerator()
+	@CsvSource(
+		"true, true",
+		"true, false",
+		"false, true",
+		"false, false",
+	)
+	@ParameterizedTest fun `empty data (no variants)`(isPrintEmptyColumns: Boolean, isPrintEmptyRows: Boolean) {
+		val sut = TableGenerator(isPrintEmptyColumns = isPrintEmptyColumns, isPrintEmptyRows = isPrintEmptyRows)
 
 		val result = sut.build(
 			mapOf(
@@ -123,8 +152,9 @@ class TableGeneratorTest {
 		assertEquals(expected, result)
 	}
 
-	@Test fun `empty data (no parsers)`() {
-		val sut = TableGenerator()
+	@ValueSource(booleans = [true, false])
+	@ParameterizedTest fun `empty data (no parsers) - expanded`(isPrintEmptyColumns: Boolean) {
+		val sut = TableGenerator(isPrintEmptyColumns = isPrintEmptyColumns, isPrintEmptyRows = true)
 
 		val result = sut.build(
 			mapOf(
@@ -139,6 +169,79 @@ class TableGeneratorTest {
 			module1	variant1${"  "}
 			module2	variant2${"  "}
 			module3	variant3${"  "}
+			Summary	(total: 0)
+		""".trimIndent().normalizeLineEndings()
+		assertEquals(expected, result)
+	}
+
+	@ValueSource(booleans = [true, false])
+	@ParameterizedTest fun `empty data (no parsers) - collapsed`(isPrintEmptyColumns: Boolean) {
+		val sut = TableGenerator(isPrintEmptyColumns = isPrintEmptyColumns, isPrintEmptyRows = false)
+
+		val result = sut.build(
+			mapOf(
+				"module1" to mapOf("variant1" to emptyMap()),
+				"module2" to mapOf("variant2" to emptyMap()),
+				"module3" to mapOf("variant3" to emptyMap())
+			)
+		)
+
+		val expected = """
+			module 	variant${"   "}
+			Summary	(total: 0)
+		""".trimIndent().normalizeLineEndings()
+		assertEquals(expected, result)
+	}
+
+	@Test fun `empty data (no counts) - expanded`() {
+		val sut = TableGenerator(isPrintEmptyColumns = true, isPrintEmptyRows = true)
+
+		val result = sut.build(NO_COUNTS)
+
+		val expected = """
+			module 	variant   	checker
+			module1	variant1  	    N/A
+			module2	variant2  	    N/A
+			module3	variant3  	    N/A
+			Summary	(total: 0)	    N/A
+		""".trimIndent().normalizeLineEndings()
+		assertEquals(expected, result)
+	}
+
+	@Test fun `empty data (no counts) - no rows`() {
+		val sut = TableGenerator(isPrintEmptyColumns = true, isPrintEmptyRows = false)
+
+		val result = sut.build(NO_COUNTS)
+
+		val expected = """
+			module 	variant   	checker
+			Summary	(total: 0)	    N/A
+		""".trimIndent().normalizeLineEndings()
+		assertEquals(expected, result)
+	}
+
+	@Test fun `empty data (no counts) - no columns`() {
+		val sut = TableGenerator(isPrintEmptyColumns = false, isPrintEmptyRows = true)
+
+		val result = sut.build(NO_COUNTS)
+
+		val expected = """
+			module 	variant${"   "}
+			module1	variant1${"  "}
+			module2	variant2${"  "}
+			module3	variant3${"  "}
+			Summary	(total: 0)
+		""".trimIndent().normalizeLineEndings()
+		assertEquals(expected, result)
+	}
+
+	@Test fun `empty data (no counts) - collapsed`() {
+		val sut = TableGenerator(isPrintEmptyColumns = false, isPrintEmptyRows = false)
+
+		val result = sut.build(NO_COUNTS)
+
+		val expected = """
+			module 	variant${"   "}
 			Summary	(total: 0)
 		""".trimIndent().normalizeLineEndings()
 		assertEquals(expected, result)
