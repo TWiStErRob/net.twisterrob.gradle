aade3264a665b7d4038ec25542582c9f53789eb1
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-01-18 00:43:33 +0000

    Reapply "Update Android Gradle Plugin from 8.13.2 to 9.0.0 (major v9)"
    This reverts commit eb9aa800508be680148d1f6f4735d5b6a020528c.

--- compat/agp/src/main/kotlin/net/twisterrob/gradle/android/CommonExtension.kt
+++ compat/agp/src/main/kotlin/net/twisterrob/gradle/android/CommonExtension.kt
@@ -2,4 +2,4 @@ package net.twisterrob.gradle.android
 
 import com.android.build.api.dsl.CommonExtension
 
-typealias CommonExtension = CommonExtension<*, *, *, *, *, *>
+typealias CommonExtension = CommonExtension
