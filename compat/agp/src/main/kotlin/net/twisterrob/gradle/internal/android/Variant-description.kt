d35f90f1ac7498bbec7a207891d8a71a467cb0bf
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-01-20 19:56:59 +0000

    Suppress MemberExtensionConflict on intentional usages

--- compat/agp/src/main/kotlin/net/twisterrob/gradle/internal/android/Variant-description.kt
+++ compat/agp/src/main/kotlin/net/twisterrob/gradle/internal/android/Variant-description.kt
@@ -7,4 +7,5 @@ import com.android.build.api.variant.impl.VariantImpl
  * Internal accessor for [VariantImpl.description] to polyfill old Variant API.
  */
 val Variant.description: String
+	@Suppress("MemberExtensionConflict") // It's fine, that's the point.
 	get() = unwrapCast<Variant, VariantImpl<*>>().description
--- test/src/main/kotlin/net/twisterrob/gradle/test/testkit/withJvmArguments.kt
+++ test/src/main/kotlin/net/twisterrob/gradle/test/testkit/withJvmArguments.kt
@@ -4,9 +4,11 @@ import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.internal.DefaultGradleRunner
 
 fun GradleRunner.withJvmArguments(vararg args: String) {
+	@Suppress("MemberExtensionConflict") // It's fine, that's the point.
 	(this as DefaultGradleRunner).withJvmArguments(*args)
 }
 
 fun GradleRunner.withJvmArguments(args: List<String>) {
+	@Suppress("MemberExtensionConflict") // It's fine, that's the point.
 	(this as DefaultGradleRunner).withJvmArguments(args)
 }
