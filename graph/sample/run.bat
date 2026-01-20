3b02705630795bb21946d4db0db0abf40ae08a95
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-01-20 19:13:27 +0000

    migrate to new

--- graph/sample/run.bat
+++ graph/sample/run.bat
@@ -1,1 +1,1 @@
-..\gradlew.bat -p . --continue --no-daemon -x lint :app:testR :a:aR --exclude-task compileRShaders %* 
+..\gradlew.bat -p . --continue --no-daemon -x lint :app:testR :a:aR --exclude-task generateRResources %* 
