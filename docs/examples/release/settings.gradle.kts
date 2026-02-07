be15162c7d8241af5c12f01ba969d59770b7b2cb
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-02-07 14:36:27 +0000

    revert to default

--- docs/examples/release/settings.gradle.kts
+++ docs/examples/release/settings.gradle.kts
@@ -4,17 +4,6 @@ pluginManagement {
 		//gradlePluginPortal() // not used
 		exclusiveContent {
 			forRepository {
-				maven("https://ossrh-staging-api.central.sonatype.com/service/local/repositories/net.twisterrob--a6fd9564-39a2-402f-ab53-147cd57ff0b3/content/") {
-					name = "Sonatype Staging for net.twisterrob"
-				}
-			}
-			filter {
-				includeVersionByRegex("""^net\.twisterrob\.gradle$""", ".*", "^${Regex.escape("0.0.1")}$")
-				includeVersionByRegex("""^net\.twisterrob\.gradle\.plugin\.[^.]+$""", ".*", "^${Regex.escape("0.0.1")}$")
-			}
-		}
-		exclusiveContent {
-			forRepository {
 				google()
 			}
 			filter {
@@ -28,23 +17,12 @@ pluginManagement {
 }
 
 plugins {
-	id("net.twisterrob.gradle.plugin.settings") version "0.0.1"
+	id("net.twisterrob.gradle.plugin.settings") version "0.18"
 }
 
 dependencyResolutionManagement {
 	repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
 	repositories {
-		exclusiveContent {
-			forRepository {
-				maven("https://ossrh-staging-api.central.sonatype.com/service/local/repositories/net.twisterrob--a6fd9564-39a2-402f-ab53-147cd57ff0b3/content/") {
-					name = "Sonatype Staging for net.twisterrob"
-				}
-			}
-			filter {
-				includeVersionByRegex("""^net\.twisterrob\.gradle$""", ".*", "^${Regex.escape("0.0.1")}$")
-				includeVersionByRegex("""^net\.twisterrob\.gradle\.plugin\.[^.]+$""", ".*", "^${Regex.escape("0.0.1")}$")
-			}
-		}
 		google()
 		mavenCentral()
 	}
