f4423fb6db2a32f275b0c0448d7c8540c753394b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-09-12 12:52:31 +0100

    Retire #5331 Detekt TODEL in 1.22.0
    Fixed by https://github.com/detekt/detekt/pull/5332.
    
    https://github.com/detekt/detekt/issues/5331

--- quality/src/main/kotlin/net/twisterrob/gradle/quality/report/html/XMLStreamWriterDSL.kt
+++ quality/src/main/kotlin/net/twisterrob/gradle/quality/report/html/XMLStreamWriterDSL.kt
@@ -49,7 +49,7 @@ inline fun XMLStreamWriter.element(
 		writeEndElement()
 	}
 
-@Suppress("detekt.CanBeNonNullable") // TODEL https://github.com/detekt/detekt/issues/5331
+@Suppress("detekt.CanBeNonNullable") // Nullable input is this convenience API's purpose.
 inline fun <T : Any> XMLStreamWriter.optionalElement(
 	name: String,
 	value: T?,
@@ -78,7 +78,7 @@ inline fun XMLStreamWriter.attribute(name: String, value: Any) {
 	writeAttribute(name, value.toString())
 }
 
-@Suppress("detekt.CanBeNonNullable") // TODEL https://github.com/detekt/detekt/issues/5331
+@Suppress("detekt.CanBeNonNullable") // Nullable input is this convenience API's purpose.
 inline fun <T : Any> XMLStreamWriter.optionalAttribute(
 	name: String,
 	value: T?,
