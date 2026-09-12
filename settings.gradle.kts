567b2a49bac7e45b4593711960543ca4a4e9fa6c
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-09-12 12:02:42 +0100

    Remove obsolete Detekt deprecation suppression

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -125,17 +125,6 @@ dependencyResolutionManagement {
 
 val gradleVersion: String = GradleVersion.current().version
 
-// TODEL Gradle 9.1 vs detekt 1.23.8 https://github.com/detekt/detekt/issues/8452
-@Suppress("detekt.MaxLineLength")
-doNotNagAbout(
-	"The ReportingExtension.file(String) method has been deprecated. " +
-			"This is scheduled to be removed in Gradle 10. " +
-			"Please use the getBaseDirectory().file(String) or getBaseDirectory().dir(String) method instead. " +
-			"Consult the upgrading guide for further information: " +
-			"https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_9.html#reporting_extension_file",
-	"at io.gitlab.arturbosch.detekt.DetektPlugin.apply(DetektPlugin.kt:28)",
-)
-
 // TODEL Gradle 9.1 vs IDEA 2025.2.1 https://youtrack.jetbrains.com/issue/IDEA-379286
 @Suppress("detekt.MaxLineLength")
 doNotNagAbout(
