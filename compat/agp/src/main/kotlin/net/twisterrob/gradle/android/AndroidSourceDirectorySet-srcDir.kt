d158814903bc2a50dd8edab4f524909fdc450821
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-09-21 00:54:51 +0100

    Inline obsolete AGP compatibility layers

--- compat/agp/src/main/kotlin/net/twisterrob/gradle/android/AndroidSourceDirectorySet-srcDir.kt
+++
@@ -1,7 +0,0 @@
-package net.twisterrob.gradle.android
-
-import com.android.build.api.dsl.AndroidSourceDirectorySet
-
-fun AndroidSourceDirectorySet.srcDirCompat(srcDir: String) {
-	this.directories.add(srcDir)
-}
--- compat/agp/src/main/kotlin/net/twisterrob/gradle/android/Sources-manifests.kt
+++
@@ -1,8 +0,0 @@
-package net.twisterrob.gradle.android
-
-import com.android.build.api.variant.Sources
-import org.gradle.api.file.RegularFile
-import org.gradle.api.provider.Provider
-
-val Sources.manifestsCompat: Provider<out List<RegularFile>>
-	get() = this.manifests.all
