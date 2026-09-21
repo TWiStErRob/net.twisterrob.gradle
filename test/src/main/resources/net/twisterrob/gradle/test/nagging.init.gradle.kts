1d1163f0a9054c639a12db3907ebcc25edf373cf
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-09-21 01:58:01 +0100

    Remove obsolete pre-2.2 KGP branches
    Keep KotlinVersions and the injected KGP property for future version work, but make every retained matrix entry use the Kotlin 2.2.21 floor. Suppressions for KGP 1.x and 2.0 can no longer activate and only add maintenance cost.

--- test/src/main/resources/net/twisterrob/gradle/test/nagging.init.gradle.kts
+++ test/src/main/resources/net/twisterrob/gradle/test/nagging.init.gradle.kts
@@ -22,47 +22,6 @@ apply<net.twisterrob.gradle.nagging.NaggingPluginForTest>()
 
 // Below nagging suppressions are sorted by (Gradle version, AGP version) lexicographically.
 
-if ("@net.twisterrob.test.kotlin.pluginVersion@" < "1.9.0") {
-	// https://youtrack.jetbrains.com/issue/KT-52976
-	// > Configure project : in all Android tests
-	// Example test: AndroidBuildPluginIntgTest.`can disable buildConfig decoration (debug)`
-	doNotNagAboutStackForTest(
-		"8.2" to "8.15",
-		"0.0" to "100.0",
-		"The org.gradle.api.plugins.Convention type has been deprecated. This is scheduled to be removed in Gradle 9.0. Consult the upgrading guide for further information: https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#deprecated_access_to_conventions",
-		"at org.jetbrains.kotlin.gradle.plugin.sources.android.configurator.GradleConventionAddKotlinSourcesToAndroidSourceSetConfigurator.configure(GradleConventionAddKotlinSourcesToAndroidSourceSetConfigurator.kt:30)",
-	)
-	// > Configure project : in all Kotlin tests
-	// Example test: KotlinPluginIntgTest.`can compile Kotlin`
-	doNotNagAboutStackForTest(
-		"8.2" to "8.15",
-		"0.0" to "100.0",
-		"The org.gradle.api.plugins.Convention type has been deprecated. This is scheduled to be removed in Gradle 9.0. Consult the upgrading guide for further information: https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#deprecated_access_to_conventions",
-		"at org.jetbrains.kotlin.gradle.plugin.AbstractKotlinPlugin\$Companion\$setUpJavaSourceSets\$1.invoke(AbstractKotlinPlugin.kt:257)",
-	)
-}
-
-if ("@net.twisterrob.test.kotlin.pluginVersion@" in "1.5.20".."2.0.10") {
-	// https://youtrack.jetbrains.com/issue/KT-67838
-	// > Configure project :
-	// Example test: AndroidBuildPluginIntgTest.`can disable buildConfig decoration (debug)`
-	// Introduced in 1.5.20: https://github.com/JetBrains/kotlin/commit/34e0a3caa890246946ec5fc0153a0b3dc4271244
-	val lineNumber = when ("@net.twisterrob.test.kotlin.pluginVersion@") {
-		in "1.8.0".."1.8.22" -> 406
-		in "1.9.0".."1.9.10" -> 417
-		in "1.9.20".."1.9.20" -> 418
-		in "1.9.21".."1.9.25" -> 422
-		in "2.0.0".."2.0.10" -> 405
-		else -> error("Don't know the line number yet for @net.twisterrob.test.kotlin.pluginVersion@.")
-	}
-	doNotNagAboutStackForTest(
-		"8.8" to "8.15",
-		"0.0" to "100.0",
-		"The Configuration.fileCollection(Spec) method has been deprecated. This is scheduled to be removed in Gradle 9.0. Use Configuration.getIncoming().artifactView(Action) with a componentFilter instead. Consult the upgrading guide for further information: https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#deprecate_filtered_configuration_file_and_filecollection_methods",
-		"at org.jetbrains.kotlin.gradle.internal.Kapt3GradleSubplugin\$createKaptKotlinTask\$2.invoke(Kapt3KotlinGradleSubplugin.kt:${lineNumber})",
-	)
-}
-
 doNotNagAboutPatternForTest(
 	"8.8" to "8.13",
 	"8.2" to "8.3",
@@ -154,20 +113,6 @@ doNotNagAboutStackForTest(
 	"Retrieving attribute with a null key. This behavior has been deprecated. This will fail with an error in Gradle 10. Don't request attributes from attribute containers using null keys. Consult the upgrading guide for further information: https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#null-attribute-lookup",
 	"at com.android.build.gradle.internal.ide.dependencies.ArtifactUtils.isAndroidProjectDependency(ArtifactUtils.kt:5",
 )
-
-if ("@net.twisterrob.test.kotlin.pluginVersion@" in "1.9.0".."1.9.25") {
-	// https://youtrack.jetbrains.com/issue/KT-64355
-	// https://youtrack.jetbrains.com/issue/KT-61457 was fixed, but then its fix deprecated again.
-	// Fixed in 2.0.0: https://github.com/JetBrains/kotlin/commit/e4050724190c4d4a25ed3aea7a26e7535bfa3e9a
-	// > Configure project : in all Kotlin tests
-	// Example test: KotlinPluginIntgTest.`can compile Kotlin`
-	doNotNagAboutStackForTest(
-		"9.0" to "9.1",
-		"0.0" to "100.0",
-		"The StartParameter.isConfigurationCacheRequested property has been deprecated. This is scheduled to be removed in Gradle 10.0. Please use 'configurationCache.requested' property on 'BuildFeatures' service instead. Consult the upgrading guide for further information: https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#deprecated_startparameter_is_configuration_cache_requested",
-		"at org.jetbrains.kotlin.gradle.plugin.internal.DefaultConfigurationCacheStartParameterAccessor\$isConfigurationCacheRequested\$2.invoke(ConfigurationCacheStartParameterAccessor.kt:35)",
-	)
-}
 
 // Gradle 9.1 vs AGP 8.13 https://issuetracker.google.com/issues/444260628
 @Suppress("detekt.MaxLineLength")
